52ecbd1dcdc7f28235cf584b2614cb3f731baedd
Joe Anderson <joejbajoe@gmail.com>
2018-12-07 20:54:23 +0000

    Put movement script on player

---
+++ Pop Pixie/Assets/Core Scripts/PlayerMovable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovable : MonoBehaviour {
+
+  public float speed;
+
+  private Rigidbody2D rb;
+
+  void Start() {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+    rb.velocity = movement * speed;
+  }
+}
