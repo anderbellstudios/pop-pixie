a27b7ae079636e4e9c214854e64532549d3f694c
Joe Anderson <joejbajoe@gmail.com>
2018-12-16 10:14:57 +0000

    Track player HP in script

---
+++ Pop Pixie/Assets/Core Scripts/PlayerHitPoints.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitPoints : MonoBehaviour {
+
+  public float Maximum; 
+  private float Current; 
+
+  void Cap () {
+    // Make sure HP is between 0 and max
+    Current = Mathf.Clamp( Current, 0, Maximum );
+  }
+
+  float Set (float val) {
+    Current = val;
+    Cap();
+    return Current;
+  }
+
+  float Increase (float val) {
+    Current += val;
+    return Current;
+  }
+
+  float Decrease (float val) {
+    return Increase(-val);
+  }
+
+	// Use this for initialization
+	void Start () {
+    Current = Maximum;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
