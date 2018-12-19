5b98c9e38163c0092d17eeba3ecba6f62cfc6459
Joe Anderson <joejbajoe@gmail.com>
2018-12-19 16:04:41 +0000

    Add static bar to HUD

---
+++ Pop Pixie/Assets/Core Scripts/HUDBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDBar : MonoBehaviour {
+
+  public Vector2 Position;
+  public Vector2 Size;
+  public Color BackgroundColour;
+  public Color ForegroundColour;
+  public float Progress;
+
+	void OnGUI () {
+    GUI.BeginGroup(
+      new Rect( Position.x, Position.y, Size.x, Size.y )
+    );
+
+    DrawBar(BackgroundColour);
+    DrawBar(ForegroundColour, Progress);
+
+    GUI.EndGroup();
+	}
+
+  private void DrawBar (Color colour, float scaleX = 1.00f) {
+    var texture = new Texture2D(1, 1);
+    texture.SetPixel(0, 0, colour);
+    texture.Apply();
+
+    GUI.skin.box.normal.background = texture;
+
+    GUI.Box(
+      new Rect( 0, 0, Size.x * scaleX, Size.y ),
+      GUIContent.none
+    );
+  }
+}
