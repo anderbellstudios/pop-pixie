e299bba193ce111035b14f8333a7417db2589020
Joe Anderson <joejbajoe@gmail.com>
2019-01-01 16:17:49 +0000

    Track Engaged flag and last active time in script

---
+++ Pop Pixie/Assets/Core Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+  public float ActivationRadius;
+  // Making all attributes public for debugging purposes;
+  // Should be changed later to avoid clutter in the inspector
+  public bool Engaged;
+  public DateTime LastActive;
+
+	// Use this for initialization
+	void Start () {
+    Engaged = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Debug.Log("Clever AI procedure goes here!");
+	}
+}
