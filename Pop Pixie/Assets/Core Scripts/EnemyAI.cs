22a656efae48e6ba23140e629db5404816e6e16b
Joe Anderson <joejbajoe@gmail.com>
2019-01-01 16:39:42 +0000

    Calculate distance to the player

--- Pop Pixie/Assets/Core Scripts/EnemyAI.cs
+++ Pop Pixie/Assets/Core Scripts/EnemyAI.cs
@@ -24,10 +24,30 @@ public class EnemyAI : MonoBehaviour {
 	void Update () {
     // Make the cool down appear in the inspector
     DebugCoolDownTimer = CoolDownTimer();
+
+    if ( Engaged ) {
+      Debug.Log("I am Engaged");
+    } else {
+      Debug.Log( DistanceToTarget() );
+    }
 	}
 
   private double CoolDownTimer() {
     // Seconds since last active
     return DateTime.Now.Subtract( LastActive ).TotalSeconds;
+  }
+
+  private float DistanceToTarget() {
+    var target = GameObject.FindGameObjectWithTag("Player");
+
+    if (target == null) {
+      Debug.Log("Target not found!");
+      return float.PositiveInfinity;
+    }
+
+    return Vector2.Distance(
+      this.transform.position,
+      target.transform.position
+    );
   }
 }
