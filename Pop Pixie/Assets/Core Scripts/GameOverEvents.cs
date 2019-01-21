fbbed265a294e2d031cce310ec7d3c0fcdf93256
Joe Anderson <joejbajoe@gmail.com>
2019-01-21 20:48:09 +0000

    Create game over screen

---
+++ Pop Pixie/Assets/Core Scripts/GameOverEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverEvents : MonoBehaviour {
+
+  public void TryAgain() {
+    GameDataController.Current.Load();
+  }
+
+  public void QuitGame() {
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+  }
+
+}
