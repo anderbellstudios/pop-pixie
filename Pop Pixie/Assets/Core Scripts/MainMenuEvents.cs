4533eb1a681b91c6934deb0c5b1186eefb91d0f4
Joe Anderson <joejbajoe@gmail.com>
2019-01-21 14:00:29 +0000

    Link button click to events in script

---
+++ Pop Pixie/Assets/Core Scripts/MainMenuEvents.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuEvents : MonoBehaviour {
+
+  public void NewGame() {
+    Debug.Log("You pressed New Game");
+  }
+
+  public void Continue() {
+    Debug.Log("You pressed Continue");
+  }
+
+}
