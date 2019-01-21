e5419441537e20169428e6f81f0af382e0beec3c
Joe Anderson <joejbajoe@gmail.com>
2019-01-21 16:10:22 +0000

    Save game data file on new game

--- Pop Pixie/Assets/Core Scripts/GameDataController.cs
+++ Pop Pixie/Assets/Core Scripts/GameDataController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using UnityEngine;
 
 public class GameDataController : MonoBehaviour {
@@ -16,5 +19,22 @@ public class GameDataController : MonoBehaviour {
     var gameData = new GameData();
     gameData.LevelId = 1;
     gameData.MakeCurrent();
+
+    Save();
+  }
+
+  public void Save () {
+    Debug.Log("Attempting to save");
+    Debug.Log( GameDataPath() );
+
+    var gameData = GameData.Current;
+    var bf = new BinaryFormatter();
+    var file = File.Open( GameDataPath(), FileMode.OpenOrCreate );
+    bf.Serialize(file, gameData);
+    file.Close();
+  }
+
+  private string GameDataPath () {
+    return Application.persistentDataPath + "/game_data.dat";
   }
 }
