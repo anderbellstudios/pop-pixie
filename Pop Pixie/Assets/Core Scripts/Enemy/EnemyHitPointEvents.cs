d30a7b43fb2f6e5523ed857bfadf9e0784dde79b
Joe Anderson <joejbajoe@gmail.com>
2019-01-22 18:57:44 +0000

    Bullets damage enemies until they die

---
+++ Pop Pixie/Assets/Core Scripts/Enemy/EnemyHitPointEvents.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitPointEvents : MonoBehaviour, IHitPointEvents {
+  public void Decreased (HitPoints hp) {
+  }
+
+  public void BecameZero (HitPoints hp) {
+    Destroy(gameObject);
+  }
+}
--- Pop Pixie/Assets/Core Scripts/Player/BulletCollidesWithEnemy.cs
+++ Pop Pixie/Assets/Core Scripts/Player/BulletCollidesWithEnemy.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class BulletCollidesWithEnemy : MonoBehaviour {
 
+  public float Damage;
+
   void OnCollisionEnter2D (Collision2D col) {
     Destroy(gameObject);
 
-    if ( col.gameObject.tag == "Enemy" ) {
-      Destroy(col.gameObject);
+    var obj = col.gameObject;
+    if ( obj.tag == "Enemy" ) {
+      obj.GetComponent<HitPoints>().Decrease( Damage );
     }
   }
 
