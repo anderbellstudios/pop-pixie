344c2fa16951b4d99504221ee5025ee75fa91cc0
Joe Anderson <joejbajoe@gmail.com>
2019-01-22 19:18:58 +0000

    Flash enemy sprite on damage

--- Pop Pixie/Assets/Core Scripts/Enemy/EnemyHitPointEvents.cs
+++ Pop Pixie/Assets/Core Scripts/Enemy/EnemyHitPointEvents.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHitPointEvents : MonoBehaviour, IHitPointEvents {
   public void Updated (HitPoints hp) {
   }
 
   public void Decreased (HitPoints hp) {
+    StartCoroutine( Flash() );
+  }
+
+  private IEnumerator Flash() {
+    var renderer = gameObject.GetComponent<SpriteRenderer>();
+
+    for(var n = 0; n < 5; n++) {
+      renderer.enabled = true;
+      yield return new WaitForSeconds(0.1f);
+      renderer.enabled = false;
+      yield return new WaitForSeconds(0.1f);
+    }
+
+    renderer.enabled = true;
   }
 
   public void BecameZero (HitPoints hp) {
