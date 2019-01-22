88c065ec6658c3558a9d121c4f3d1f3d1eb37f76
Joe Anderson <joejbajoe@gmail.com>
2019-01-22 18:48:06 +0000

    Shoot bullets from player straight up

---
+++ Pop Pixie/Assets/Core Scripts/Player/BulletCollidesWithEnemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollidesWithEnemy : MonoBehaviour {
+
+  void OnCollisionEnter2D (Collision2D col) {
+    Destroy(gameObject);
+  }
+
+}
---
+++ Pop Pixie/Assets/Core Scripts/Player/BulletEmitter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEmitter : MonoBehaviour {
+
+  public GameObject prefab;
+  public float Speed;
+
+	// Use this for initialization
+	void Start () {
+    InvokeRepeating("Shoot", 0.0f, 0.5f);
+	}
+	
+	void Shoot () {
+    var direction = new Vector3( 0, 1, 0 ).normalized;
+    var origin = gameObject.transform.position + ( 1 * direction );
+
+    var bullet = Instantiate(
+      prefab, 
+      origin,
+      Quaternion.identity
+    );
+
+    bullet.GetComponent<Rigidbody2D>().velocity = Speed * direction;
+	}
+}
