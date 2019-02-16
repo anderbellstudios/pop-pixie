64d225b146d73e1e2b139f78c3ba9067d1a92358
Joe Anderson <joejbajoe@gmail.com>
2019-02-16 09:03:18 +0000

    Make cursor a crosshair

---
+++ Pop Pixie/Assets/Core Scripts/UI/CrosshairCursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairCursor : MonoBehaviour {
+
+  public Texture2D CrosshairTexture;
+
+	// Update is called once per frame
+	void Update () {
+    Cursor.SetCursor(
+      CrosshairTexture, 
+      Vector2.zero, 
+      CursorMode.Auto
+    );
+	}
+}
