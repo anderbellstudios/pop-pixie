ba38efc8158898aa8e92d9df6f3059ffecf55179
Joe Anderson <joejbajoe@gmail.com>
2019-03-03 20:46:55 +0000

    Display dialogue text with typewriter effect

--- Pop Pixie/Assets/Core Scripts/UI/DialogueBoxController.cs
+++ Pop Pixie/Assets/Core Scripts/UI/DialogueBoxController.cs
@@ -6,10 +6,50 @@ using UnityEngine.UI;
 public class DialogueBoxController : MonoBehaviour {
 
   public Text TextBox;
+  public float InitialDelay;
+  public float WriteDelay;
+
+  [Multiline]
+  public string DebugText;
+
+  private string FullText;
+  private int WriteProgress;
+
+  public void Write (string text) {
+    FullText = text;
+    WriteProgress = 0;
+    DirectWrite("");
+
+    InvokeRepeating(
+      "WriteNextLetter",
+      InitialDelay,
+      WriteDelay
+    );
+  }
+
+  void WriteNextLetter () {
+    if ( WriteProgress == FullText.Length ) {
+      CancelInvoke();
+      return;
+    }
+
+    WriteProgress += 1;
+
+    DirectWrite(
+      FullText.Substring(
+        0,
+        WriteProgress
+      )
+    );
+  }
+
+  void DirectWrite (string text) {
+    TextBox.text = text;
+  }
 
 	// Use this for initialization
 	void Start () {
-    TextBox.text = "Hello world!";
+    Write(DebugText);
 	}
 	
 	// Update is called once per frame
