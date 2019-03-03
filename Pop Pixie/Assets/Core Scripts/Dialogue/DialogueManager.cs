e9e043d16916faeeeaa86844f8bb40c4fda3a646
Joe Anderson <joejbajoe@gmail.com>
2019-03-03 21:44:36 +0000

    Advance dialogue automatically

--- Pop Pixie/Assets/Core Scripts/Dialogue/DialogueManager.cs
+++ Pop Pixie/Assets/Core Scripts/Dialogue/DialogueManager.cs
@@ -7,13 +7,23 @@ public class DialogueManager : MonoBehaviour, IDialogueEventHandler {
   public DialogueBoxController DialogueBox;
 
   private DialogueSequence Sequence;
+  private int SequenceProgress;
 
   void ReadPage (DialoguePage page) {
     DialogueBox.Write( page.Text );
   }
 
   public void PageFinished () {
-    Debug.Log("Finished page");
+    ReadNextPage();
+  }
+
+  void ReadNextPage () {
+    if ( SequenceProgress < Sequence.Pages.Length ) {
+      var page = Sequence.Pages[ SequenceProgress ];
+      ReadPage(page);
+
+      SequenceProgress += 1;
+    }
   }
 
 	// Use this for initialization
@@ -22,8 +32,9 @@ public class DialogueManager : MonoBehaviour, IDialogueEventHandler {
 
     string json = Resources.Load<TextAsset>("Dialogue/l1d1").text;
     Sequence = DialogueSequence.ParseJSON(json);
+    SequenceProgress = 0;
 
-    ReadPage( Sequence.Pages[0] );
+    ReadNextPage();
 	}
 	
 	// Update is called once per frame
