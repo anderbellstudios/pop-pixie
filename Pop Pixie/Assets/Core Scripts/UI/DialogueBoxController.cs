b54845028a15544a39a701e3c50eb0f5b6e300e0
Joe Anderson <joejbajoe@gmail.com>
2019-03-03 20:31:02 +0000

    Place dialogue box at bottom of screen

---
+++ Pop Pixie/Assets/Core Scripts/UI/DialogueBoxController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBoxController : MonoBehaviour {
+
+  public Text TextBox;
+
+	// Use this for initialization
+	void Start () {
+    TextBox.text = "Hello world!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
