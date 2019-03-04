e13d7af6ad6a298d278b43f9259752c2a887e8c9
Joe Anderson <joejbajoe@gmail.com>
2019-03-04 17:49:33 +0000

    Play elevator dialogue on collision with elevator

--- Pop Pixie/Assets/Level Scripts/Level1Elevator.cs
+++ Pop Pixie/Assets/Level Scripts/Level1Elevator.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level1Elevator : MonoBehaviour {
+public class Level1Elevator : MonoBehaviour, IDialogueSequenceEventHandler {
+
+  public DialogueManager Dialogue;
+
   void OnTriggerEnter2D (Collider2D other) {
     if ( other.tag == "Player" ) {
-      Debug.Log("Wheeeeee");
+      StateManager.SetState( State.Dialogue );
+      Dialogue.Play("Dialogue/l1d3", this);
     }
+  }
+
+  public void SequenceFinished () {
+    Debug.Log("And now, elevator happens.");
   }
 }
