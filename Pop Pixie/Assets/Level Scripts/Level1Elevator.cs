d6ffee1eba73cbe22518c3db746c3355960aa9ff
Joe Anderson <joejbajoe@gmail.com>
2019-03-04 17:42:18 +0000

    Detect collision with elevator door

---
+++ Pop Pixie/Assets/Level Scripts/Level1Elevator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Elevator : MonoBehaviour {
+  void OnTriggerEnter2D (Collider2D other) {
+    if ( other.tag == "Player" ) {
+      Debug.Log("Wheeeeee");
+    }
+  }
+}
