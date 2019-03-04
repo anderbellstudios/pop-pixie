001b5579d02bda3f28c8c858fcbef29bded56f29
Joe Anderson <joejbajoe@gmail.com>
2019-03-04 18:20:21 +0000

    Create test lore item with collision detection

---
+++ Pop Pixie/Assets/Level Scripts/Level1TestLoreItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1TestLoreItem : MonoBehaviour {
+
+  void OnTriggerEnter2D (Collider2D other) {
+    if ( other.tag == "Player" ) {
+      Debug.Log("And now, lore happens!");
+    }
+  }
+}
