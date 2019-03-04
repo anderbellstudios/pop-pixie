b53885101e0be3a97e97d575970659d15ddfd00d
Joe Anderson <joejbajoe@gmail.com>
2019-03-04 17:01:02 +0000

    Play encounter dialogue when gremlin gets close

---
+++ Pop Pixie/Assets/Level Scripts/Level1EnemyClose.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1EnemyClose : MonoBehaviour, IDialogueSequenceEventHandler {
+
+  public DialogueManager Dialogue;
+  public bool Triggered;
+  public GameObject Enemy;
+  public GameObject Pixie;
+  public float ActivationRadius;
+
+	void Update () {
+    if ( Triggered )
+      return;
+
+    var dist = Vector3.Distance(
+      Enemy.transform.position,
+      Pixie.transform.position
+    );
+
+    if ( dist < ActivationRadius ) {
+      Triggered = true;
+      StateManager.SetState( State.Dialogue );
+      Dialogue.Play("Dialogue/l1d2", this);
+    }
+	}
+
+  public void SequenceFinished () {
+    StateManager.SetState( State.Playing );
+  }
+}
