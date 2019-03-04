67ce7dbfde3a6c52f95ab9cf92c77f0d4db75fc9
Joe Anderson <joejbajoe@gmail.com>
2019-03-04 17:30:03 +0000

    Open elevator barrier when enemy is killed

---
+++ Pop Pixie/Assets/Level Scripts/Level1EnemiesKilled.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Level1EnemiesKilled : MonoBehaviour {
+
+  public List<GameObject> Enemies;
+  public bool Triggered;
+  public EdgeCollider2D ElevatorBarrier;
+
+	// Update is called once per frame
+	void Update () {
+    if (Triggered)
+      return;
+
+    bool all_dead = Enemies.All(
+      enemy => enemy == null
+    );
+
+    if (all_dead) {
+      Triggered = true;
+      ElevatorBarrier.enabled = false;
+    }
+	}
+}
