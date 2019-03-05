ba801ddb225e353c91f4ca1391caad29f68a7292
Joe Anderson <joejbajoe@gmail.com>
2019-03-05 10:38:44 +0000

    Create lore window controller and hide lore window

---
+++ Pop Pixie/Assets/Core Scripts/Lore/LoreWindowController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoreWindowController : MonoBehaviour {
+
+  public Text TextBox;
+  public GameObject LoreWindow;
+
+  public void Write (string text) {
+    TextBox.text = text;
+  }
+
+  public void Show () {
+    SetEnabled(true);
+  }
+
+  public void Hide () {
+    SetEnabled(false);
+  }
+
+  void SetEnabled (bool state) {
+    LoreWindow.SetActive(state);
+  }
+	// Use this for initialization
+	void Start () {
+    Hide();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
