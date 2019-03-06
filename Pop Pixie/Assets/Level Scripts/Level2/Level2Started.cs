a7617a769f71832fa68c4abcd23bda57dea39786
Joe Anderson <joejbajoe@gmail.com>
2019-03-06 18:21:59 +0000

    Upgrade project to Unity 2018.3.7

---
+++ Pop Pixie/Assets/Level Scripts/Level2/Level2Started.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Started : MonoBehaviour, IDialogueSequenceEventHandler {
+
+  public ScreenFade Fader;
+
+	// Use this for initialization
+	void Start () {
+    Fader.Fade("from black", 2.0f);
+    StateManager.SetState( State.Playing );
+	}
+
+  public void SequenceFinished () {
+  }
+}
