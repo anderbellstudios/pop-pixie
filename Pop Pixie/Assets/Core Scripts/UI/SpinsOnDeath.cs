5be6751cce9bada9e0ee6b1c5f094c75532709ce
Joe Anderson <joejbajoe@gmail.com>
2019-03-06 13:29:44 +0000

    Spin and zoom camera on death

---
+++ Pop Pixie/Assets/Core Scripts/UI/SpinsOnDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinsOnDeath : MonoBehaviour {
+
+  public float ZoomSpeed;
+  public float SpinSpeed;
+  public Camera Cam;
+
+	// Update is called once per frame
+	void Update () {
+    if ( StateManager.Isnt( State.Dying ) )
+      return;
+
+    Cam.orthographicSize -= ZoomSpeed * Time.deltaTime;
+    gameObject.transform.Rotate( 0, 0, SpinSpeed * Time.deltaTime );
+	}
+}
