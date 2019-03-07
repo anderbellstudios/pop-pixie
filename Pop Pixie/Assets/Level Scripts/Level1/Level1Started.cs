19f520cff0b26a794e52d26cc7ea7c2a658d7207
Joe Anderson <joejbajoe@gmail.com>
2019-03-07 18:38:23 +0000

    Play 'Shall we begin?' in first and second levels

--- Pop Pixie/Assets/Level Scripts/Level1/Level1Started.cs
+++ Pop Pixie/Assets/Level Scripts/Level1/Level1Started.cs
@@ -6,10 +6,12 @@ public class Level1Started : MonoBehaviour, IDialogueSequenceEventHandler {
 
   public DialogueManager Dialogue;
   public ScreenFade Fader;
+  public AudioClip Music;
 
 	// Use this for initialization
 	void Start () {
     Fader.Fade("from black", 2.0f);
+    MusicController.Current.Play(Music, "level 1");
     Dialogue.Play("Dialogue/l1d1", this);
 	}
 
--- Pop Pixie/Assets/Level Scripts/Level2/Level2Started.cs
+++ Pop Pixie/Assets/Level Scripts/Level2/Level2Started.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Level2Started : MonoBehaviour, IDialogueSequenceEventHandler {
 
   public ScreenFade Fader;
+  public AudioClip Music;
 
 	// Use this for initialization
 	void Start () {
     Fader.Fade("from black", 2.0f);
+    MusicController.Current.Play(Music, "level 1");
     StateManager.SetState( State.Playing );
 	}
 
