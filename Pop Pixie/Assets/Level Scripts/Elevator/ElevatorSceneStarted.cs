38b53f10c59fa44c41f1848679494fdee6cffdca
Joe Anderson <joejbajoe@gmail.com>
2019-03-08 16:43:42 +0000

    Fade smoothly from level 1 to elevator to level 2

---
+++ Pop Pixie/Assets/Level Scripts/Elevator/ElevatorSceneStarted.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorSceneStarted : MonoBehaviour {
+
+  public ScreenFade Fader;
+
+  // Start is called before the first frame update
+  void Start() {
+    Fader.Fade("from black", 2.0f);
+    MusicController.Current.Fade(1.0f, 0.0f, 1.0f);
+
+    Invoke("FadeToNextLevel", 8.0f);
+  }
+
+  void FadeToNextLevel () {
+    Fader.Fade("to black", 4.0f);
+    Invoke("NextLevel", 5.0f);
+  }
+
+  void NextLevel () {
+    GameDataController.Current.NextLevel();
+  }
+
+}
