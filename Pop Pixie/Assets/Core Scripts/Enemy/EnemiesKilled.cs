fd5bc4bd642e19b4962e7cb7eb8ccf2b77bf2737
Joe Anderson <joejbajoe@gmail.com>
2019-03-08 14:53:57 +0000

    Add end of game dialogue prompt to final level

--- Pop Pixie/Assets/Core Scripts/Enemy/EnemiesKilled.cs
+++ Pop Pixie/Assets/Core Scripts/Enemy/EnemiesKilled.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Level1EnemiesKilled : MonoBehaviour {
+public class EnemiesKilled : MonoBehaviour {
 
   public List<GameObject> Enemies;
   public bool Triggered;
---
+++ Pop Pixie/Assets/Level Scripts/Level2/Level2Elevator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Elevator : MonoBehaviour, IDialogueSequenceEventHandler, IPromptButtonEventHandler {
+
+  public DialogueManager Dialogue;
+  public DialoguePromptManager PromptManager;
+  public ScreenFade Fader;
+
+  void OnTriggerEnter2D (Collider2D other) {
+    if ( other.tag == "Player" ) {
+      Dialogue.Play("Dialogue/l2d3", this);
+    }
+  }
+
+  public void SequenceFinished () {
+    ShowPrompt();
+  }
+
+  void ShowPrompt () {
+    PromptManager.Display(
+      "(It appears that your journey must end here for now.)\n(Will you depart?)",
+      "Depart",
+      "Not yet",
+      this
+    );
+  }
+
+  public void ButtonPressed (string button) {
+    if ( button == "positive" ) {
+      Fader.Fade("to black", 2.0f);
+    }
+  }
+
+}
