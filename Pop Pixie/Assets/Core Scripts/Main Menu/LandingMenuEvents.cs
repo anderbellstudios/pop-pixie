a2a6e4578eddaa8b96e8327df2e354e4834fd88b
Joe Anderson <joejbajoe@gmail.com>
2019-03-09 11:09:55 +0000

    Create pre-main menu screen

---
+++ Pop Pixie/Assets/Core Scripts/Main Menu/LandingMenuEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LandingMenuEvents : MonoBehaviour {
+
+  public AudioClip Music;
+  public ScreenFade Fader;
+
+  void Start () {
+    Fader.Fade("to black", 0.0f);
+    MusicController.Current.Play(Music, "landing menu");
+    Invoke("FadeIn", 4.0f);
+  }
+
+  void FadeIn () {
+    Fader.Fade("from black", 8.0f);
+  }
+
+  public void Begin() {
+    MusicController.Current.Fade(1.0f, 0.0f, 4.0f);
+    Fader.Fade("to black", 4.0f);
+    Invoke("MainMenu", 5.0f);
+  }
+
+  void MainMenu () {
+    SceneManager.LoadScene("Main Menu");
+  }
+
+}
