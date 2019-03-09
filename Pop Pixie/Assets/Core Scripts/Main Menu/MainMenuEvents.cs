23f697a9f63c3e8369ca021029c793ba74585476
Joe Anderson <joejbajoe@gmail.com>
2019-03-09 11:20:48 +0000

    Fade main menu in and out

--- Pop Pixie/Assets/Core Scripts/Main Menu/MainMenuEvents.cs
+++ Pop Pixie/Assets/Core Scripts/Main Menu/MainMenuEvents.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class MainMenuEvents : MonoBehaviour {
 
   public AudioClip Music;
+  public ScreenFade Fader;
 
   void Start () {
     MusicController.Current.Play(Music, "main menu");
+    Fader.Fade("from black", 2.0f);
   }
 
-  public void NewGame() {
+  public void NewGame () {
+    FadeOutAndRun("NewGameInstant");
+  }
+
+  void NewGameInstant () {
     GameDataController.Current.NewGame();
   }
 
-  public void Continue() {
+  public void Continue () {
+    FadeOutAndRun("ContinueInstant");
+  }
+
+  public void ContinueInstant () {
     GameDataController.Current.Load();
+  }
+
+  void FadeOutAndRun (string callback) {
+    Fader.Fade("to black", 3.0f);
+    MusicController.Current.Fade(1.0f, 0.0f, 3.0f);
+    Invoke(callback, 4.0f);
   }
 
 }
