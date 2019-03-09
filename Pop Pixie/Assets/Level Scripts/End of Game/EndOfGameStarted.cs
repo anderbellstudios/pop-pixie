d370b62f773d3438fe5e70e81a5fbd7bb814929b
Joe Anderson <joejbajoe@gmail.com>
2019-03-09 11:59:30 +0000

    Add music to 'thanks for playing' screen

--- Pop Pixie/Assets/Level Scripts/End of Game/EndOfGameStarted.cs
+++ Pop Pixie/Assets/Level Scripts/End of Game/EndOfGameStarted.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class EndOfGameStarted : MonoBehaviour {
 
+  public AudioClip Music;
   public ScreenFade Fader;
 
   void Start () {
     Fader.Fade("from black", 3.0f);
+    Invoke("PlayMusic", 3.0f);
+  }
+
+  void PlayMusic () {
+    MusicController.Current.Play(Music, "thanks for playing");
   }
 }
