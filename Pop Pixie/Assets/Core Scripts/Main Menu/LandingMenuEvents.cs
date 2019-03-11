891ebb7169f8409c3bbabf73e4d9c5381b60fae3
Joe Anderson <joejbajoe@gmail.com>
2019-03-11 19:45:19 +0000

    Wait for fade in before enabling button

--- Pop Pixie/Assets/Core Scripts/Main Menu/LandingMenuEvents.cs
+++ Pop Pixie/Assets/Core Scripts/Main Menu/LandingMenuEvents.cs
@@ -7,8 +7,10 @@ public class LandingMenuEvents : MonoBehaviour {
 
   public AudioClip Music;
   public ScreenFade Fader;
+  public MonoBehaviour Button;
 
   void Start () {
+    DisableButton();
     Fader.Fade("to black", 0.0f);
     MusicController.Current.Play(Music, "landing menu");
     Invoke("FadeIn", 4.0f);
@@ -16,9 +18,12 @@ public class LandingMenuEvents : MonoBehaviour {
 
   void FadeIn () {
     Fader.Fade("from black", 8.0f);
+    Invoke("EnableButton", 8.0f);
   }
 
+
   public void Begin() {
+    DisableButton();
     MusicController.Current.Fade(1.0f, 0.0f, 4.0f);
     Fader.Fade("to black", 4.0f);
     Invoke("MainMenu", 5.0f);
@@ -28,4 +33,11 @@ public class LandingMenuEvents : MonoBehaviour {
     SceneManager.LoadScene("Main Menu");
   }
 
+  void EnableButton () {
+    Button.enabled = true;
+  }
+
+  void DisableButton () {
+    Button.enabled = false;
+  }
 }
