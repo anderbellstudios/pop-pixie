573f87c681f329d9784e86ea4f89df98ec6207c1
Joe Anderson <joejbajoe@gmail.com>
2019-03-11 18:18:08 +0000

    Play random noises from some gremlins

---
+++ Pop Pixie/Assets/Core Scripts/Sounds/PassiveSounds.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveSounds : MonoBehaviour {
+  public SoundController SoundPlayer;
+  public List<AudioClip> Sounds;
+  public float MinInterval, MaxInterval;
+
+  private DateTime LastPlayed;
+  private float PlayInterval;
+
+  void Start () {
+    UpdatePlayInterval();
+  }
+
+  void Update () {
+    if ( ShouldPlay() ) {
+      // Because C# doesn't appear to have any built-in 
+      // random sampling methods. 
+      int i = UnityEngine.Random.Range(0, Sounds.Count);
+      var sound = Sounds[i];
+      SoundPlayer.Play(sound);
+
+      UpdatePlayInterval();
+    }
+  }
+
+  bool ShouldPlay () {
+    var since = DateTime.Now.Subtract( LastPlayed ).TotalSeconds;
+    return since > PlayInterval;
+  }
+
+  void UpdatePlayInterval () {
+    LastPlayed = DateTime.Now;
+
+    PlayInterval = MinInterval 
+      + ( MaxInterval - MinInterval ) 
+      * ( UnityEngine.Random.value );
+  }
+}
---
+++ Pop Pixie/Assets/Core Scripts/Sounds/SoundController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+  public AudioSource Player;
+
+  public void Play (AudioClip sound) {
+    Player.clip = sound;
+    Player.Play();
+  }
+}
