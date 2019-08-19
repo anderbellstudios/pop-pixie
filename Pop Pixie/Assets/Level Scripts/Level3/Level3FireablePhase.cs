0b9d1419e94d95d0dcdac45b8294ba72ae80d1b3
Joe Anderson <joejbajoe@gmail.com>
2019-08-19 12:39:01 +0100

    Make turrets damageable

--- Pop Pixie/Assets/Level Scripts/Level3/Level3FireablePhase.cs
+++ Pop Pixie/Assets/Level Scripts/Level3/Level3FireablePhase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level3LaserPhase : APhase {
+public class Level3FireablePhase : APhase {
 
   public FireableScheduler FireableScheduler;
 
