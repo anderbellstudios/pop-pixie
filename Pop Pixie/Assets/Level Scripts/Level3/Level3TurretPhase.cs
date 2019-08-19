9a73242b4b9f239182b02d618b585a8f213a43ec
Joe Anderson <joejbajoe@gmail.com>
2019-08-19 11:19:57 +0100

    Add turret which turns to point at player

---
+++ Pop Pixie/Assets/Level Scripts/Level3/Level3TurretPhase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3TurretPhase : APhase {
+
+	public override void LocalBegin () {
+    Debug.Log("Turrets!");
+  }
+
+  public override void WhilePhaseRunning() {
+  }
+
+}
---
+++ Pop Pixie/Assets/Level Scripts/Level3/LooksAtPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooksAtPlayer : MonoBehaviour {
+
+  public GameObject player;
+  public float k, w_squared;
+  public float Velocity;
+
+	void Update () {
+    Accelerate( w_squared * RelativeAngle() );
+    Accelerate( -k * Velocity );
+
+    VelocityTick();
+	}
+
+  void Accelerate( float amount ) {
+    Velocity += amount * Time.deltaTime;
+  }
+
+  void VelocityTick() {
+    transform.Rotate( 0, 0, Velocity * Time.deltaTime );
+  }
+
+  float RelativeAngle() {
+    return Mathf.Deg2Rad * Vector3.SignedAngle(
+      transform.right, 
+      TargetDirection(), 
+      Vector3.forward
+    );
+  }
+
+  Vector3 TargetDirection() {
+    return player.transform.position - transform.position;
+  }
+}
