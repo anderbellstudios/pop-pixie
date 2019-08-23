c9a62e80a1877a0ac4a8ea89c042cfcea80b44ab
Joe Anderson <joejbajoe@gmail.com>
2019-08-23 12:48:27 +0100

    Keep as not working when destroyed after load

--- Pop Pixie/Assets/Level Scripts/Level3/Level3StopWorkingWhenDestroyed.cs
+++ Pop Pixie/Assets/Level Scripts/Level3/Level3StopWorkingWhenDestroyed.cs
@@ -10,13 +10,22 @@ public class Level3StopWorkingWhenDestroyed : MonoBehaviour, IHitPointEvents {
   public AFireable Fireable;
   public List<Behaviour> DisableComponents;
 
+  public bool StoppedWorking = false;
+
   public void Updated (HitPoints hp) {
+    if ( !StoppedWorking && hp.Current == 0 )
+      StopWorking();
   }
 
   public void Decreased (HitPoints hp) {
   }
 
   public void BecameZero (HitPoints hp) {
+  }
+
+  void StopWorking() {
+    StoppedWorking = true;
+
     FireableScheduler.RemoveFireable( Fireable );
     DisableComponents.ForEach( comp => comp.enabled = false );
   }
