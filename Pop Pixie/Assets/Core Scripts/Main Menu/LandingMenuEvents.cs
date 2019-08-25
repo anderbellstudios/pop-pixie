c057374a2d3a4adb534be59646b53188ef2a754f
Joe Anderson <joejbajoe@gmail.com>
2019-08-25 10:11:52 +0100

    Redesign UI in landing menu

--- Pop Pixie/Assets/Core Scripts/Main Menu/LandingMenuEvents.cs
+++ Pop Pixie/Assets/Core Scripts/Main Menu/LandingMenuEvents.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LandingMenuEvents : MonoBehaviour {
 
   public AudioClip Music;
   public ScreenFade Fader;
-  public MonoBehaviour Button;
 
   public float FadeInDelay, FadeInDuration, FadeOutDuration, NextSceneDelay;
 
   void Start () {
-    DisableButton();
     Fader.Fade("to black", 0.0f);
     MusicController.Current.Play(Music, "landing menu");
     Invoke("FadeIn", FadeInDelay);
@@ -20,12 +19,10 @@ public class LandingMenuEvents : MonoBehaviour {
 
   void FadeIn () {
     Fader.Fade("from black", FadeInDuration);
-    Invoke("EnableButton", FadeInDuration);
   }
 
 
   public void Begin() {
-    DisableButton();
     MusicController.Current.Fade(1.0f, 0.0f, FadeOutDuration);
     Fader.Fade("to black", FadeOutDuration);
     Invoke("MainMenu", FadeOutDuration + NextSceneDelay);
@@ -33,13 +30,5 @@ public class LandingMenuEvents : MonoBehaviour {
 
   void MainMenu () {
     SceneManager.LoadScene("Main Menu");
-  }
-
-  void EnableButton () {
-    Button.enabled = true;
-  }
-
-  void DisableButton () {
-    Button.enabled = false;
   }
 }
