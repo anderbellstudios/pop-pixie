434ab6f32af13123991b76d4b42f21325bb628ad
Joe Anderson <joejbajoe@gmail.com>
2019-08-26 09:14:18 +0100

    Refactor fade and music logic into GenericMenuEvents

---
+++ Pop Pixie/Assets/Core Scripts/Menu/LandingMenuEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LandingMenuEvents : GenericMenuEvents {
+
+  public void Begin() {
+    FadeOut(MainMenu);
+  }
+
+  void MainMenu () {
+    SceneManager.LoadScene("Main Menu");
+  }
+
+}
