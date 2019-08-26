42f83af847ab72d22b2be6715eb9c5cfa1beb123
Joe Anderson <joejbajoe@gmail.com>
2019-08-26 09:26:37 +0100

    Update game over screen to use GenericMenuEvents

---
+++ Pop Pixie/Assets/Core Scripts/Menu/GameOverEvents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverEvents : GenericMenuEvents {
+
+  public void TryAgain() {
+    FadeOut(_TryAgain);
+  }
+
+  public void QuitGame() {
+    FadeOut(_QuitGame);
+  }
+
+  void _TryAgain () {
+    GameDataController.Current.Load();
+  }
+
+  void _QuitGame() {
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+  }
+
+}
