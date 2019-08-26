599bc3365af3e7b7ffb63e0ac7c9a268df226194
Joe Anderson <joejbajoe@gmail.com>
2019-08-26 09:19:34 +0100

    Update main menu to use GenericMenuEvents

--- Pop Pixie/Assets/Core Scripts/Menu/MainMenuEvents.cs
+++ Pop Pixie/Assets/Core Scripts/Menu/MainMenuEvents.cs
@@ -2,36 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuEvents : MonoBehaviour {
-
-  public AudioClip Music;
-  public ScreenFade Fader;
-
-  void Start () {
-    MusicController.Current.Play(Music, "main menu");
-    Fader.Fade("from black", 2.0f);
-  }
+public class MainMenuEvents : GenericMenuEvents {
 
   public void NewGame () {
-    FadeOutAndRun("NewGameInstant");
-  }
-
-  void NewGameInstant () {
-    GameDataController.Current.NewGame();
+    FadeOut(_NewGame);
   }
 
   public void Continue () {
-    FadeOutAndRun("ContinueInstant");
+    FadeOut(_Continue);
   }
 
-  public void ContinueInstant () {
+  void _NewGame () {
+    GameDataController.Current.NewGame();
+  }
+
+  void _Continue () {
     GameDataController.Current.Load();
-  }
-
-  void FadeOutAndRun (string callback) {
-    Fader.Fade("to black", 3.0f);
-    MusicController.Current.Fade(1.0f, 0.0f, 3.0f);
-    Invoke(callback, 4.0f);
   }
 
 }
