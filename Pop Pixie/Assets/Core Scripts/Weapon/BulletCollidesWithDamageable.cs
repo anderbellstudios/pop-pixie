7d6a58917a29bff086439535b4b6106d3783803c
Joe Anderson <joejbajoe@gmail.com>
2019-09-06 10:29:45 +0100

    Bullet trails linger and exposion spawns on hit

--- Pop Pixie/Assets/Core Scripts/Weapon/BulletCollidesWithDamageable.cs
+++ Pop Pixie/Assets/Core Scripts/Weapon/BulletCollidesWithDamageable.cs
@@ -7,13 +7,35 @@ public class BulletCollidesWithDamageable : MonoBehaviour {
   public float Damage;
 
   void OnCollisionEnter2D (Collision2D col) {
-    Destroy(gameObject);
+    ImmoboliseBullet();
+    SpawnExplosion();
+    Invoke( "DestroyBullet", 0.5f );
 
     var hp = col.gameObject.GetComponent<HitPoints>();
 
     if ( hp != null ) {
       hp.Damage( Damage );
     }
+  }
+
+  void ImmoboliseBullet() {
+    GetComponent<SpriteRenderer>().enabled = false;
+    GetComponent<Collider2D>().enabled = false;
+    GetComponent<Rigidbody2D>().simulated = false;
+    GetComponent<TrailRenderer>().emitting = false;
+  }
+
+  void SpawnExplosion() {
+    var explosion = (GameObject) Resources.Load(
+      "Bullets/Explosion", 
+      typeof(GameObject)
+    );
+
+    Instantiate(explosion, transform);
+  }
+
+  void DestroyBullet() {
+    Destroy(gameObject);
   }
 
 }
---
+++ Pop Pixie/Assets/Core Scripts/Weapon/Explosion.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Explosion : MonoBehaviour {
+
+  public Image Image;
+  public float Duration;
+
+  IntervalTimer Timer;
+
+  void Start() {
+    Timer = new IntervalTimer() {
+      Interval = Duration
+    };
+
+    Timer.Reset();
+  }
+
+  void Update() {
+    transform.localScale = new Vector3( Scale(), Scale(), Scale() );
+
+    var colour = Image.color;
+    colour.a = Alpha();
+    Image.color = colour;
+
+    if ( Timer.Elapsed() )
+      Destroy(gameObject);
+  }
+
+  float Scale() {
+    return Mathf.Lerp( 0.2f, 1f, Progress() );
+  }
+
+  float Alpha() {
+    return Mathf.Lerp( 1f, 0.75f, Progress() );
+  }
+
+  float Progress() {
+    return Timer.TimeSinceElapsed() / Duration;
+  }
+
+}
