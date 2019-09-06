f9ce243bb505af0c55fce96169c59ab6ed9d34c3
Joe Anderson <joejbajoe@gmail.com>
2019-09-06 13:26:53 +0100

    Reimplement HUD bars using canvas images

--- Pop Pixie/Assets/Core Scripts/UI/HUDBar.cs
+++ Pop Pixie/Assets/Core Scripts/UI/HUDBar.cs
@@ -1,50 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDBar : MonoBehaviour {
 
-  public Vector2 Position;
-  public Vector2 Size;
-  public Color BackgroundColour;
-  public Color ForegroundColour;
+  public Image FilledImage;
+  public List<Image> AllImages;
+
   public float Progress;
   public bool Visible;
 
-	void OnGUI () {
-    if ( !Visible )
-      return;
-
-    GUI.BeginGroup(
-      new Rect( 
-        Screen.width  * Position.x, 
-        Screen.height * Position.y, 
-        Screen.width  * Size.x, 
-        Screen.height * Size.y 
-      )
-    );
-
-    DrawBar(BackgroundColour);
-    DrawBar(ForegroundColour, Progress);
-
-    GUI.EndGroup();
-	}
-
-  private void DrawBar (Color colour, float scaleX = 1.00f) {
-    var texture = new Texture2D(1, 1);
-    texture.SetPixel(0, 0, colour);
-    texture.Apply();
-
-    GUI.skin.box.normal.background = texture;
-
-    GUI.Box(
-      new Rect( 
-        0, 
-        0, 
-        Screen.width  * Size.x * scaleX, 
-        Screen.height * Size.y 
-      ),
-      GUIContent.none
-    );
+  void Update() {
+    AllImages.ForEach( img => img.enabled = Visible );
+    FilledImage.fillAmount = Progress;
   }
+
 }
