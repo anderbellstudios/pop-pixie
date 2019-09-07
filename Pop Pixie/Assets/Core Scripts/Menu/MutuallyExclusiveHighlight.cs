206b695da1bef6db57f1df108ce0ec6c7e983c41
Joe Anderson <joejbajoe@gmail.com>
2019-09-07 10:02:50 +0100

    Fix: Hovered button stays highlighted when other buttons are selected

--- Pop Pixie/Assets/Core Scripts/Menu/MutuallyExclusiveHighlight.cs
+++ Pop Pixie/Assets/Core Scripts/Menu/MutuallyExclusiveHighlight.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MutuallyExclusiveHighlight : MonoBehaviour {
+public class MutuallyExclusiveHighlight : MonoBehaviour, IDeselectHandler {
   public void OnPointerEnter() {
-    var button = GetComponent<Button>();
-
-    button.Select();
-    button.OnSelect(null);
+    Button().Select();
+    Button().OnSelect(null);
   }
+
+  public void OnDeselect( BaseEventData eventData ) {
+    // Create and trigger an artificial OnPointerExit event to turn off highlighting
+    Button().OnPointerExit( eventData as PointerEventData );
+  }
+
+  Button Button() {
+    return GetComponent<Button>();
+  }
+
 }
