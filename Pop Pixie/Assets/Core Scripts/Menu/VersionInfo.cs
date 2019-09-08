0272c1791c7aa9554fe0add59f73bef3b39857cf
Joe Anderson <joejbajoe@gmail.com>
2019-09-08 09:05:23 +0100

    Show current and latest version numbers on landing

---
+++ Pop Pixie/Assets/Core Scripts/Menu/VersionInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionInfo : MonoBehaviour {
+
+  public Text Current, Latest;
+  public string VersionURL;
+
+  void Awake() {
+    Current.text = Application.version;
+
+    InvokeRepeating( "FetchLatestVersion", 0f, 3f );
+  }
+
+  void FetchLatestVersion() {
+    try {
+
+      using ( WebClient client = new WebClient() ) {
+       Latest.text = client.DownloadString( VersionURL );
+       CancelInvoke();
+      }
+
+    } catch ( WebException e ) {
+      Debug.Log(e);
+    }
+  }
+
+}
