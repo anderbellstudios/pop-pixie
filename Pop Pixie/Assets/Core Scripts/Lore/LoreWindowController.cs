315737fa85e3fbb5658957adef4cb0f433af6ba9
Joe Anderson <joejbajoe@gmail.com>
2019-09-19 17:09:26 +0100

    Fix: Lore items have large empty space under text

--- Pop Pixie/Assets/Core Scripts/Lore/LoreWindowController.cs
+++ Pop Pixie/Assets/Core Scripts/Lore/LoreWindowController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class LoreWindowController : MonoBehaviour {
 
-  public Text TextBox;
+  public TMP_Text TextBox;
+  public RectTransform TextBoxTransform;
   public GameObject LoreWindow;
   public GameObject ScrollBar;
 
   public void Write (string text) {
     TextBox.text = text;
+    TextBoxTransform.sizeDelta = new Vector2(
+      0,
+      TextBox.GetPreferredValues().y
+    );
   }
 
   public void Show () {
