95427cfad2236ec58434e6581d70943094587b6d
Joe Anderson <joejbajoe@gmail.com>
2019-09-27 18:45:27 +0100

    Do not run the same attack twice in a row

--- Pop Pixie/Assets/Core Scripts/Enemy/RandomAIAI.cs
+++ Pop Pixie/Assets/Core Scripts/Enemy/RandomAIAI.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class RandomAIAI : AEnemyAI {
 
   public List<AEnemyAI> AIs;
+  public int LastAI = -1;
 
   public override void ControlGained() {
     RelinquishControlTo( RandomAI() );
   }
 
   AEnemyAI RandomAI() {
-    var i = Random.Range(0, AIs.Count);
+    int i = LastAI;
+
+    while ( i == LastAI ) {
+      i = Random.Range(0, AIs.Count);
+    }
+
+    LastAI = i;
+
     return AIs[i];
   }
 
