4a8f5a547ce3315784eb4343a9dcf792b7afdc36
Joe Anderson <joejbajoe@gmail.com>
2019-12-18 11:59:28 +0000

    Flying ring pull flies off in random direction

---
+++ Pop Pixie/Assets/Core Scripts/Ring Pulls/FlyingRingPull.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingRingPull : MonoBehaviour {
+
+  public Rigidbody2D rb;
+  public float InitialSpeed;
+
+  void Start() {
+    rb.velocity = InitialSpeed * Random.insideUnitCircle.normalized;
+  }
+
+}
