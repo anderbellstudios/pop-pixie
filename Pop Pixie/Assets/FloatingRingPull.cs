4202b2031e041537647ed471f45b4299c81c6d87
Joe Anderson <joejbajoe@gmail.com>
2019-12-18 11:47:57 +0000

    Move ring pull into canvas space on collide with player

---
+++ Pop Pixie/Assets/FloatingRingPull.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingRingPull : MonoBehaviour {
+
+  public SpawnFlyingRingPull SpawnFlyingRingPull;
+
+  void OnTriggerEnter2D( Collider2D col ) {
+    if ( col.tag == "Player" ) {
+      SpawnFlyingRingPull.Instantiate();
+      Destroy(gameObject);
+    }
+  }
+
+}
---
+++ Pop Pixie/Assets/SpawnFlyingRingPull.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFlyingRingPull : MonoBehaviour {
+
+  public GameObject Prefab;
+  public Transform Parent; 
+  public Camera Camera;
+
+  public void Instantiate() {
+    GameObject flyingRingPull = Instantiate(Prefab, Parent);
+    flyingRingPull.transform.position = Camera.WorldToScreenPoint(transform.position);
+  }
+
+}
