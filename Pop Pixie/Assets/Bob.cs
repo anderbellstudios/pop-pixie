ed57c1fc854a7e423acdd394f55c7dda60ebdcd9
Joe Anderson <joejbajoe@gmail.com>
2019-12-18 11:19:24 +0000

    Bob rings pulls up and down

---
+++ Pop Pixie/Assets/Bob.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour {
+
+  public float Speed;
+  public float Amplitude;
+
+  void Update() {
+    transform.localPosition = new Vector3(
+      0,
+      Amplitude * Mathf.Sin(Speed * Time.time),
+      0
+    );
+  }
+
+}
