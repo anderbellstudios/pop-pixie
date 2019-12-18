6403bd819039b22e1a8717a8c8ffbd2ea1f699ab
Joe Anderson <joejbajoe@gmail.com>
2019-12-18 13:33:49 +0000

    Randomise initial rotation and bob of ring pulls

--- Pop Pixie/Assets/Core Scripts/Misc/Bob.cs
+++ Pop Pixie/Assets/Core Scripts/Misc/Bob.cs
@@ -6,11 +6,20 @@ public class Bob : MonoBehaviour {
 
   public float Speed;
   public float Amplitude;
+  public bool RandomInitialPhase;
+
+  float PhaseOffset = 0;
+
+  void Start() {
+    if ( RandomInitialPhase ) {
+      PhaseOffset = Random.Range(0, 360);
+    }
+  }
 
   void Update() {
     transform.localPosition = new Vector3(
       0,
-      Amplitude * Mathf.Sin(Speed * Time.time),
+      Amplitude * Mathf.Sin(Speed * Time.time + PhaseOffset),
       0
     );
   }
--- Pop Pixie/Assets/Core Scripts/Misc/Revolve.cs
+++ Pop Pixie/Assets/Core Scripts/Misc/Revolve.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Revolve : MonoBehaviour {
 
   public float Speed;
+  public bool RandomInitialRotation;
+
+  void Start() {
+    if ( RandomInitialRotation ) {
+      gameObject.transform.Rotate( 0, Random.Range(0, 360), 0 );
+    }
+  }
 
   void Update() {
     gameObject.transform.Rotate(0, Speed * Time.deltaTime, 0);
