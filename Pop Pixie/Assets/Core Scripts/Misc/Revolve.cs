0c24e8861b01e4f380aa02968eed7fd25d7ef296
Joe Anderson <joejbajoe@gmail.com>
2019-12-18 10:58:45 +0000

    Idly rotate ring pull around Y axis

---
+++ Pop Pixie/Assets/Core Scripts/Misc/Revolve.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolve : MonoBehaviour {
+
+  public float Speed;
+
+  void Update() {
+    gameObject.transform.Rotate(0, Speed * Time.deltaTime, 0);
+  }
+
+}
