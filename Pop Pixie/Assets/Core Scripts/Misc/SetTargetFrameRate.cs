16678a1981b887878b65c3d4b7a6d9ab616cbd72
Joe Anderson <joe@mousetrapped.co.uk>
2019-12-30 18:08:04 +0000

    Fix: Particle effects look different in build

---
+++ Pop Pixie/Assets/Core Scripts/Misc/SetTargetFrameRate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTargetFrameRate : MonoBehaviour {
+
+  public int FrameRate;
+
+  void Start() {
+    Application.targetFrameRate = FrameRate;
+  }
+
+}
