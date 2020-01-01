28fed25829b943b28914313a97f386f4a5a06978
Joe Anderson <joe@mousetrapped.co.uk>
2020-01-01 17:30:54 +0000

    Respect calibration in WrappedInput

--- Pop Pixie/Assets/Core Scripts/Controls/WrappedInput.cs
+++ Pop Pixie/Assets/Core Scripts/Controls/WrappedInput.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class WrappedInput : MonoBehaviour {
 
   public static bool GetButton( string button ) {
-    var controllerButton = ControllerButtonName(button);
+    KeyCode? controllerButton = ControllerButtonKeyCode(button);
 
     if ( controllerButton != null ) {
-      if ( Input.GetButton(controllerButton) )
+      if ( Input.GetKey( (KeyCode) controllerButton ) )
         return true;
     }
 
@@ -17,23 +17,18 @@ public class WrappedInput : MonoBehaviour {
   }
 
   public static bool GetButtonDown( string button ) {
-    var controllerButton = ControllerButtonName(button);
+    KeyCode? controllerButton = ControllerButtonKeyCode(button);
 
     if ( controllerButton != null ) {
-      if ( Input.GetButtonDown(controllerButton) )
+      if ( Input.GetKeyDown( (KeyCode) controllerButton ) )
         return true;
     }
 
     return Input.GetButtonDown( KeyboardButtonName(button) );
   }
 
-  static string ControllerButtonName( string button ) {
-    var prefix = ControllerPrefix();
-
-    if ( prefix != null )
-      return ControllerPrefix() + " " + button;
-
-    return null;
+  static KeyCode? ControllerButtonKeyCode( string button ) {
+    return GamePadButtonData.GetKeyCode( button );
   }
 
   static string KeyboardButtonName( string button ) {
