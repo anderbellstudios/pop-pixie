81b549019a66ddc80ff038c06d470eeeffa649e7
Joe Anderson <joe@mousetrapped.co.uk>
2020-03-22 15:35:02 +0000

    Configure WrappedInput to use on Rewired

--- Pop Pixie/Assets/Core Scripts/Controls/WrappedInput.cs
+++ Pop Pixie/Assets/Core Scripts/Controls/WrappedInput.cs
@@ -3,43 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using Rewired;
 
 public class WrappedInput : MonoBehaviour {
 
+  private static Player Player {
+    get => ReInput.players.GetPlayer(0);
+  }
+
   public static bool GetButton( string buttonName ) {
-    GamePadButton button = GamePadButtonData.GetButton( buttonName );
-    if ( button != null && button.GetButton() ) return true;
-    return Input.GetButton( KeyboardButtonName(buttonName) );
+    return Player.GetButton( buttonName );
   }
 
   public static bool GetButtonDown( string buttonName ) {
-    GamePadButton button = GamePadButtonData.GetButton( buttonName );
-    // Debug.Log(buttonName);
-    // Debug.Log(button);
-    if ( button != null && button.GetButtonDown() ) return true;
-    return Input.GetButtonDown( KeyboardButtonName(buttonName) );
+    return Player.GetButtonDown( buttonName );
   }
 
   public static float GetAxis( string axis ) {
-    float amount;
-
-    if ( axis == "Horizontal" || axis == "Vertical" ) {
-      amount = Input.GetAxis( axis );
-    } else {
-      amount = 0;
-    }
-
-    String controllerInput = GamePadAxisData.GetInput( axis );
-    int controllerSign = GamePadAxisData.GetSign( axis );
-
-    if ( controllerInput != null ) {
-      float controllerAmount = Input.GetAxis( controllerInput ) * controllerSign;
-
-      if ( Mathf.Abs( controllerAmount ) > Mathf.Abs(amount) )
-        amount = controllerAmount;
-    }
-
-    return amount;
+    return Player.GetAxis( axis );
   }
 
   static string KeyboardButtonName( string button ) {
