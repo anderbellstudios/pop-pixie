468aa31dfde8765a7cceeed28abf13c33aa9f8c6
Joe Anderson <joe@mousetrapped.co.uk>
2020-05-08 17:58:10 +0100

    Save and resume game while on the Elevator scene

--- Pop Pixie/Assets/Level Scripts/Elevator/ElevatorEvents.cs
+++ Pop Pixie/Assets/Level Scripts/Elevator/ElevatorEvents.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ElevatorEvents : GenericMenuEvents {
+public class ElevatorEvents : GenericMenuEvents, ISerializableComponent {
 
-  public void NextLevel () {
+  public string[] SerializableFields { get; } = { "NextLevel" };
+
+  public string NextLevel;
+
+  public override void LocalStart() {
+    if (ElevatorData.NextLevel != null) {
+      NextLevel = ElevatorData.NextLevel;
+    }
+
+    SceneData.Save();
+    SaveGame.WriteSave();
+  }
+
+  public void Continue () {
     FadeOut( () => {
       GDCall.ExpectFirstTime();
-
-      SceneManager.LoadScene(
-        ElevatorData.NextLevel
-      );
+      SceneManager.LoadScene(NextLevel);
     });
   }
 
