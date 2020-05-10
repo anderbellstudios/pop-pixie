a0afefb8a9d3eff231ecdd07ad6595e772dd380b
Joe Anderson <joe@mousetrapped.co.uk>
2020-05-10 17:29:38 +0100

    Wire weapon tile data up to script

---
+++ Pop Pixie/Assets/Level Scripts/Shop/WeaponTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteInEditMode]
+public class WeaponTile : MonoBehaviour {
+
+  public Sprite Sprite;
+  public int Price;
+  public bool Bought;
+
+  public Image WeaponImage;
+  public TMP_Text PriceLabel;
+  public MonoBehaviour TickImage;
+
+  void Update() {
+    WeaponImage.sprite = Sprite;
+    PriceLabel.text = Price.ToString();
+
+    byte opacity = (byte) (Bought ? 64 : 255);
+    WeaponImage.color = new Color32(255, 255, 255, opacity);
+    TickImage.enabled = Bought;
+  }
+
+  public void ToggleBought_TESTING_ONLY() {
+    Bought = !Bought;
+  }
+
+}
