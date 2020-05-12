76eb6853dc84267093a20e4c3498f0b3d9d2883c
Joe Anderson <joe@mousetrapped.co.uk>
2020-05-12 17:52:35 +0100

    Remove redundant fields and methods on Weapon

--- Pop Pixie/Assets/Core Scripts/Weapon/Weapon.cs
+++ Pop Pixie/Assets/Core Scripts/Weapon/Weapon.cs
@@ -10,7 +10,6 @@ public class Weapon : ScriptableObject {
   public string Id;
   public float FireRate;
   public int Capacity;
-  public int Ammunition;
   public float Scatter = 0;
   public float BulletSpeed;
   public GameObject BulletPrefab;
@@ -20,18 +19,4 @@ public class Weapon : ScriptableObject {
 
   public float CooldownInterval() 
     => 1.0f / FireRate;
-
-  public bool HasBullets() 
-    => Ammunition > 0;
-
-  public void ExpendBullet() {
-    Ammunition = Mathf.Max(0, Ammunition - 1);
-  }
-
-  public bool Full() 
-    => Ammunition == Capacity;
-
-  public void Reload() {
-    Ammunition = Capacity;
-  }
 }
