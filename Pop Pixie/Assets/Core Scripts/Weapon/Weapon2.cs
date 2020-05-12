9e45e0d3a1dbaa0666c862c1bfc11d18403d5608
Joe Anderson <joe@mousetrapped.co.uk>
2020-05-12 14:10:01 +0100

    WIP Store weapon data in scriptable objects

---
+++ Pop Pixie/Assets/Core Scripts/Weapon/Weapon2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Weapon", order = 1)]
+public class NewWeapon : ScriptableObject {
+
+  public float FireRate;
+  public int Capacity;
+  public int Ammunition;
+  public float Scatter = 0;
+  public float BulletSpeed;
+  public GameObject BulletPrefab;
+  public Sprite Sprite;
+  public Sprite InHandSprite;
+  public AudioClip ShootSound;
+
+  public float CooldownInterval() 
+    => 1.0f / FireRate;
+
+  public bool HasBullets() 
+    => Ammunition > 0;
+
+  public void ExpendBullet() {
+    Ammunition = Mathf.Max(0, Ammunition - 1);
+  }
+
+  public bool Full() 
+    => Ammunition == Capacity;
+
+  public void Reload() {
+    Ammunition = Capacity;
+  }
+}
