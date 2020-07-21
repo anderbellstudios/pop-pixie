421e7658b41cf6c85f5add27a503b2a4c3803379
Joe Anderson <joe@mousetrapped.co.uk>
2020-07-21 19:01:35 +0100

    Show keycard dialogue when level is completed

--- Pop Pixie/Assets/Core Scripts/Level/LevelCompleted.cs
+++ Pop Pixie/Assets/Core Scripts/Level/LevelCompleted.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelCompleted : MonoBehaviour {
+public class LevelCompleted : MonoBehaviour, IDialogueSequenceEventHandler {
 
   public ElevatorBarrier ElevatorBarrier;
+  public DialogueManager Dialogue;
+  public float Delay = 1.5f;
 
   public void Run() {
+    Invoke("ShowMessage", Delay);
+  }
+
+  void ShowMessage() {
+    Dialogue.Play("Dialogue/Keycard", this);
+  }
+
+  public void SequenceFinished() {
     ElevatorBarrier.Remove();
-	}
+  }
 }
---
+++ Pop Pixie/Assets/Level Scripts/Level3/EventPhase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventPhase : APhase {
+
+  [SerializeField] public UnityEvent Event;
+
+	public override void LocalBegin () {
+    Event.Invoke();
+    PhaseFinished();
+  }
+
+}
