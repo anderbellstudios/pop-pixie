e183ec9a136582badc567fc23e649717c1508652
Joe Anderson <joe@mousetrapped.co.uk>
2020-08-07 17:09:22 +0100

    Fix: Keycard dialogue may play on resume

--- Pop Pixie/Assets/Core Scripts/Level/LevelCompleted.cs
+++ Pop Pixie/Assets/Core Scripts/Level/LevelCompleted.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelCompleted : MonoBehaviour, IDialogueSequenceEventHandler {
+public class LevelCompleted : MonoBehaviour, IDialogueSequenceEventHandler, ISerializableComponent {
+
+  public string[] SerializableFields { get; } = { "KeycardDialoguePlayed" };
 
   public ElevatorBarrier ElevatorBarrier;
   public DialogueManager Dialogue;
   public float Delay = 1.5f;
 
+  public bool KeycardDialoguePlayed = false;
+
   public void Run() {
-    Invoke("ShowMessage", Delay);
+    ElevatorBarrier.Remove();
+
+    if (!KeycardDialoguePlayed)
+      Invoke("ShowMessage", Delay);
   }
 
   void ShowMessage() {
+    KeycardDialoguePlayed = true;
     Dialogue.Play("Dialogue/Keycard", this);
   }
 
-  public void SequenceFinished() {
-    ElevatorBarrier.Remove();
-  }
+  public void SequenceFinished() {}
 }
