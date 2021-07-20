8c34902ff627a97500fee2b0931082d318d93286
Joe Anderson <joe@osbert.me>
2021-07-20 13:13:13 +0100

    Control fade in, fade out and scene change via SceneEvents

--- Pop Pixie/Assets/Core Scripts/Misc/SceneChangeHopper.cs
+++ Pop Pixie/Assets/Core Scripts/Misc/SceneChangeHopper.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneChangeHopper : MonoBehaviour {
   public string SceneName;
 
   public void Hop() {
-    SceneManager.LoadScene(SceneName);
+    SceneEvents.Current.ChangeScene(SceneName);
   }
 }
---
+++ Pop Pixie/Assets/Core Scripts/Misc/SceneEvents.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneEvents : MonoBehaviour {
+
+  public bool SingletonInstance = true;
+  public static SceneEvents Current;
+
+  public ScreenFade Fader;
+  public bool ShouldFadeIn = true, ShouldFadeOut = true, WaitForFadeInBeforePermittingExit = false;
+  public float FadeInDelay, FadeInDuration, FadeOutDuration, PostFadeOutDelay;
+
+  [SerializeField] public UnityEvent OnFadeIn;
+
+  private bool FadingIn = false, FadingOut = false;
+  private string NewSceneName;
+
+  void Start() {
+    if (SingletonInstance)
+      Current = this;
+
+    if (ShouldFadeIn) {
+      FadingIn = true;
+      Fader.Fade("to black", 0.0f);
+      Invoke("FadeIn", FadeInDelay);
+    }
+  }
+
+  void FadeIn() {
+    Fader.Fade("from black", FadeInDuration);
+    Invoke("AfterFadeIn", FadeInDuration);
+  }
+
+  void AfterFadeIn() {
+    FadingIn = false;
+    OnFadeIn.Invoke();
+  }
+
+  public void ChangeScene(string sceneName) {
+    if ( FadingOut || (WaitForFadeInBeforePermittingExit && FadingIn) )
+      return;
+
+    NewSceneName = sceneName;
+
+    if (ShouldFadeOut) {
+      FadingOut = true;
+
+      AudioMixer.Current.FadeOut( FadeOutDuration );
+      Fader.Fade("to black", FadeOutDuration);
+      Invoke("LoadNewScene", FadeOutDuration + PostFadeOutDelay);
+    } else {
+      LoadNewScene();
+    }
+  }
+
+  void LoadNewScene() {
+    SceneManager.LoadScene(NewSceneName);
+  }
+}
