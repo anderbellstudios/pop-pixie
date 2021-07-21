fe682899438581ed8150d58d7a61767f0c3c0c26
Joe Anderson <joe@osbert.me>
2021-07-21 19:58:04 +0100

    Play 'Now commencing' dialogue before fade in

--- Pop Pixie/Assets/Level Scripts/Training Game/Room 1/TrainingGameRoom1Events.cs
+++ Pop Pixie/Assets/Level Scripts/Training Game/Room 1/TrainingGameRoom1Events.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using UnityEngine;
 
 public class TrainingGameRoom1Events : MonoBehaviour {
+  public ScreenFade Fader;
   public List<GameObject> Targets;
 
   private bool TargetsDestroyed = false;
 
   void Start() {
+    Fader.Fade("to black", 0.0f);
+
     // Drain all available weapons to force player to reload
     PlayerWeapons.Current.AvailableWeapons().ForEach(weapon => weapon.Ammunition = 0);
 
@@ -25,5 +28,9 @@ public class TrainingGameRoom1Events : MonoBehaviour {
       ? null
       : "Aim and press <color=#00ffff>[Fire]</color> to shoot the <color=#ffff00>Hologrems</color>"
     );
+  }
+
+  public void FadeIn() {
+    Fader.Fade("from black", 1f);
   }
 }
