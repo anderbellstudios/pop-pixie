af5b711aceac3330cf1ba59338118d7b523e71b7
Joe Anderson <joe@osbert.me>
2021-08-24 17:27:47 +0100

    Add labels and pause button to HUD controls

--- Pop Pixie/Assets/Core Scripts/UI/InspectButtonImage.cs
+++ Pop Pixie/Assets/Core Scripts/UI/InspectButtonImage.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InspectButtonImage : MonoBehaviour {
 
   public Image Image;
+  public TMP_Text Label;
 
   void Update() {
-    Image.enabled = AInspectable.ShowButtonPrompt();
+    bool enabled = AInspectable.ShowButtonPrompt();
+    Image.enabled = enabled;
+    Label.enabled = enabled;
   }
 
 }
