c8a2900c9f39a42484632cadeaa5cd8b972633c5
Joe Anderson <joe@osbert.me>
2022-06-08 10:16:01 +0100

    Add progress bar to training game results screen

--- Pop Pixie/Assets/Core Scripts/Phases/DelayPhase.cs
+++ Pop Pixie/Assets/Core Scripts/Phases/DelayPhase.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class DelayPhase : APhase {
 
   public bool PauseGameplay;
+  public bool UsePlayingTime = false;
   public float Delay;
+  public HUDBar HUDBar;
+
+  private IntervalTimer Timer;
 
 	public override void LocalBegin () {
+    Timer = new IntervalTimer() {
+      TimeClass = UsePlayingTime ? "PlayingTime" : "Time",
+      Interval = Delay
+    };
+
+#if UNITY_EDITOR
+    Debug.Assert(!(UsePlayingTime && PauseGameplay), "DelayPhase cannot use playing time AND pause gameplay");
+#endif
+
     if (PauseGameplay)
       StateManager.AddState(State.NotPlaying);
 
-    Invoke("AfterDelay", Delay);
+    Timer.Reset();
   }
 
-  void AfterDelay() {
-    if (PauseGameplay)
-      StateManager.RemoveState(State.NotPlaying);
+  public override void WhilePhaseRunning() {
+    if (HUDBar != null)
+      HUDBar.Progress = Timer.Progress();
 
-    PhaseFinished();
+    Timer.IfElapsed(() => {
+      if (PauseGameplay)
+        StateManager.RemoveState(State.NotPlaying);
+
+      PhaseFinished();
+    });
   }
 
 }
