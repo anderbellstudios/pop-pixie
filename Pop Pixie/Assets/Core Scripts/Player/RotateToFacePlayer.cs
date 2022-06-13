7f02909be175321d7040551a018d424832033093
Joe Anderson <joe@mousetrapped.co.uk>
2022-06-13 09:58:21 +0100

    Add holoring sound

---
+++ Pop Pixie/Assets/Core Scripts/Player/RotateToFacePlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToFacePlayer : MonoBehaviour {
+	void Update () {
+    if (!StateManager.Playing)
+      return;
+
+    Vector3 toPlayer = PlayerGameObject.Current.transform.position - transform.position;
+
+    transform.rotation = Quaternion.Euler(
+      0,
+      0,
+      Mathf.Atan2(toPlayer.y, toPlayer.x) * Mathf.Rad2Deg
+    );
+	}
+}
