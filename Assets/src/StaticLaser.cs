76338f1e5b66f49b94a549e4d7ec15714cb95220
Joe Anderson <joe@mousetrapped.co.uk>
2022-06-28 14:45:22 +0100

    Fix: Laser hum is loudest at centre of laser beam

--- Assets/src/StaticLaser.cs
+++ Assets/src/StaticLaser.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StaticLaser : MonoBehaviour {
+  public Transform LaserHumTransform;
+  public float MinY, MaxY;
+
   public void CollidedWithPlayer() {
     PlayerGameObject.Current.GetComponent<HitPoints>().Damage(1);
+  }
+
+  void Update() {
+    if (!StateManager.Playing)
+      return;
+
+    float relativePlayerY = transform.InverseTransformPoint(PlayerGameObject.Current.transform.position).y;
+    LaserHumTransform.localPosition = new Vector3(0, Mathf.Clamp(relativePlayerY, MinY, MaxY), 0);
   }
 }
