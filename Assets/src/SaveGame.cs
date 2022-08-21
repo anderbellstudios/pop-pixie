381cc72d006b836a3d61157cea08a9d5a594e1fb
Joe Anderson <joe@mousetrapped.co.uk>
2022-08-21 11:55:04 +0100

    Document file0 and file1 in comment

--- Assets/src/SaveGame.cs
+++ Assets/src/SaveGame.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class SaveGame {
 
+  /**
+   * Pop Pixie uses two save files: file0 and file1.
+   *
+   * file0 is used to store any progress that should not be undone if the
+   * player dies, such as starting a new level or reaching a mid-level
+   * milestone.
+   *
+   * file1 is used to store autosaves, which can take place at any time.
+   *
+   * Some typical workflows involving save data are described below:
+   *
+   * Starting a new level (see SaveHopper.cs): Once all game objects have
+   * started, the level state is serialized into GameData and written to both
+   * file0 and file1.
+   *
+   * Autosaving (see SaveHopper.cs): The level state is serialized and written
+   * to file1.
+   *
+   * Choosing 'Continue' from the main menu (see ContinueGameHopper.cs): The
+   * contents of file1 are read into GameData and deserialized, which involves
+   * loading the saved level and restoring the state of all game objects.
+   *
+   * Player death (see PlayerHitPointEvents.cs): The contents of file0 are
+   * read into GameData (but not deserialized, so nothing changes onscreen)
+   * and are copied into file1. This ensures that if the player quits the game
+   * and loads the autosave (file1), the player's progress will be reverted to
+   * the latest milestone (i.e. you can't cheat death by quitting).
+   *
+   * Choosing 'Try again' from the game over screen: The contents of file0 are
+   * read into GameData and deserialized.
+   */
+
   public static void WriteSave() {
     WriteTrueSave();
     WriteAutoSave();
@@ -26,15 +58,15 @@ public class SaveGame {
   }
 
   public static bool Exists() {
-    return new DataOperation( GameData.Current, "file1" ).Exists();
+    return new DataOperation(GameData.Current, "file1").Exists();
   }
 
-  static void Write( string fileName ) {
-    new DataOperation( GameData.Current, fileName ).Write();
+  static void Write(string fileName) {
+    new DataOperation(GameData.Current, fileName).Write();
   }
 
-  static void Read( string fileName ) {
-    new DataOperation( GameData.Current, fileName ).Read();
+  static void Read(string fileName) {
+    new DataOperation(GameData.Current, fileName).Read();
   }
 
 }
