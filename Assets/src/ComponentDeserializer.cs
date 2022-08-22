d21625fc1bc36d7f14aee9abd0b48794539d0255
Joe Anderson <joe@mousetrapped.co.uk>
2022-08-22 15:33:48 +0100

    Fix: ComponentDeserializer cannot handle integers

--- Assets/src/ComponentDeserializer.cs
+++ Assets/src/ComponentDeserializer.cs
@@ -8,28 +8,34 @@ public class ComponentDeserializer : MonoBehaviour {
   SerializedComponent SerializedComponent;
   GameObject GameObject;
 
-  public ComponentDeserializer( SerializedComponent serializedComponent, GameObject gameObject ) {
+  public ComponentDeserializer(
+    SerializedComponent serializedComponent,
+    GameObject gameObject
+  ) {
     SerializedComponent = serializedComponent;
     GameObject = gameObject;
   }
 
   public void Deserialize() {
-    ISerializableComponent component = ( ISerializableComponent ) GameObject.GetComponent( SerializedComponent.Type );
+    ISerializableComponent component = (ISerializableComponent) GameObject.GetComponent(
+      SerializedComponent.Type
+    );
 
-    foreach ( string fieldName in ( component ).SerializableFields ) {
-      var field = SerializedComponent.Type.GetField( fieldName );
-      dynamic val = SerializedComponent.Fields[ fieldName ];
+    foreach (string fieldName in component.SerializableFields) {
+      var field = SerializedComponent.Type.GetField(fieldName);
+      dynamic val = SerializedComponent.Fields[fieldName];
 
-      if (field.FieldType == typeof(Single)) {
-        field.SetValue( component, (float) val );
+      if (field.FieldType == typeof(Int32)) {
+        field.SetValue(component, Convert.ToInt32(val));
+      } else if (field.FieldType == typeof(Single)) {
+        field.SetValue(component, (float) val);
       } else {
-        field.SetValue( component, val );
+        field.SetValue(component, val);
       }
 
-      if ( component is ISaveCallbacks ) {
-        ( (ISaveCallbacks) component ).AfterLoad();
+      if (component is ISaveCallbacks) {
+        ((ISaveCallbacks) component).AfterLoad();
       }
-
     }
   } 
 }
