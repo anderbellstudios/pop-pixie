abc20dc5e321043764a81323dec4a0913bbe5207
Joe Anderson <joe@mousetrapped.co.uk>
2022-08-23 14:37:14 +0100

    Redesign game over screen

--- Assets/src/GameOverEvents.cs
+++ Assets/src/GameOverEvents.cs
@@ -1,41 +1,14 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class GameOverEvents : GenericMenuEvents {
-
-  public Text AssistModeButtonText;
-
-  EnumeratorButton<AssistModeData.AssistModeHP> AssistModeButton;
-
-  public override void LocalStart() {
-    AssistModeButton = AssistModeData.MakeAssistModeButton(
-      (label) => { AssistModeButtonText.text = label; }
-    );
-  }
-
+public class GameOverEvents : MonoBehaviour {
   public void TryAgain() {
-    FadeOut(_TryAgain);
-  }
-
-  public void QuitGame() {
-    FadeOut(_QuitGame);
-  }
-
-  public void ShiftAssistMode() {
-    AssistModeButton.Shift();
-  }
-
-  void _TryAgain () {
     SaveGame.ReadSave();
-
     SceneData.Load();
   }
 
-  void _QuitGame() {
+  public void Quit() {
     WrappedApplication.Quit();
   }
-
 }
