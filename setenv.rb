7c23f944e2109939942ae186ba94c39a3a5cfcd5
Joe Anderson <joe@mousetrapped.co.uk>
2022-09-06 15:30:54 +0100

    Use 'environments' to manage EditorBuildSettings.asset

---
+++ setenv.rb
@@ -0,0 +1,28 @@
+require 'tty-prompt'
+require 'fileutils'
+
+prompt = TTY::Prompt.new
+
+environments = Dir.glob('./environments/*').select { |f| File.directory?(f) }.map { |f| File.basename(f) }
+previous_environment = File.read('./environments/current').strip
+
+new_environment = prompt.select('Switch to environment:', environments, default: previous_environment, filter: true)
+
+environment_files = Dir.glob("./environments/#{new_environment}/**/*", File::FNM_DOTMATCH)
+  .select { |f| File.file?(f) }
+  .map { |f| f.sub("./environments/#{new_environment}/", '') }
+
+environment_files.each do |file|
+  system("diff -q ./#{file} ./environments/#{previous_environment}/#{file}")
+
+  if $?.exitstatus == 1
+    $stderr.puts "Manually merge #{file} before switching environments"
+    exit 1
+  end
+end
+
+environment_files.each do |file|
+  FileUtils.cp("./environments/#{new_environment}/#{file}", "./#{file}")
+end
+
+File.write('./environments/current', new_environment)
