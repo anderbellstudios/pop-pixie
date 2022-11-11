fd32ebd91833311db7ad88013839f81a11de4aba
Joe Anderson <joe@mousetrapped.co.uk>
2022-11-11 14:24:05 +0000

    Show roll hint during holoring attack

--- Assets/src/MentoeHologramHoloringAttackAI.cs
+++ Assets/src/MentoeHologramHoloringAttackAI.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class MentoeHologramHoloringAttackAI : ARepeatedAttackAI {
   public GameObject HoloringPrefab;
 
+  void Start() {
+    InGamePrompt.Current.RegisterSource(98, () =>
+      InControl
+      ? "Press <size=150%>[Roll]</size> while moving to <color=#ffff00>roll</color>"
+      : null
+    );
+  }
+
   public override void PerformAttack() {
     Instantiate(HoloringPrefab, transform);
   }
