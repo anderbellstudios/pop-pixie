5a2aec21458c2b7c9661501450e7013434e898d6
Joe Anderson <joe@mousetrapped.co.uk>
2023-11-03 23:22:53 +0000

    Add boot text

---
+++ Assets/src/TerminalEmulatorPrintLineByLine.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TerminalEmulatorPrintLineByLine : MonoBehaviour {
+  public TerminalEmulator TerminalEmulator;
+  public bool HopOnStart = true;
+  public float DelayBetweenLines = 0.1f;
+  [TextArea] public string Content;
+  public UnityEvent OnFinish;
+
+  private List<string> Lines;
+  private int CurrentLine = 0;
+
+  void Start() {
+    if (HopOnStart)
+      Hop();
+  }
+
+  public void Hop() {
+    Lines = new List<string>(Content.Split('\n'));
+    CurrentLine = 0;
+    PrintNextLine();
+  }
+
+  void PrintNextLine() {
+    if (CurrentLine >= Lines.Count) {
+      OnFinish.Invoke();
+      return;
+    }
+
+    AsyncTimer.BaseTime.SetTimeout(() => {
+      TerminalEmulator.PrintLine(Lines[CurrentLine++]);
+      PrintNextLine();
+    }, DelayBetweenLines);
+  }
+}
