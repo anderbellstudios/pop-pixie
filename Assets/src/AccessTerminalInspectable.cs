f804a8d7ac6dc11845eeeeb55f035390511902f5
Joe Anderson <joe@mousetrapped.co.uk>
2023-11-04 19:36:09 +0000

    Integrate access terminal with HUD and in-level sprite

--- Assets/src/AccessTerminalInspectable.cs
+++ Assets/src/AccessTerminalInspectable.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AccessTerminalInspectable : AInspectable {
+  public AccessTerminalConfig Config;
+
+  private bool HasBeenAccessed = false;
+
   public override void OnInspect() {
-    Debug.Log("You inspected!");
+    HasBeenAccessed = true;
+    LoreItemData.RecordRead(Config.LoreItem);
+    StateManager.AddState(State.NotPlaying);
+
+    AccessTerminalManager.Current.Open(Config, () => {
+      LoreManager.Current.Open(Config.LoreItem, () => {
+        StateManager.RemoveState(State.NotPlaying);
+      });
+    });
   }
 
   public override string AInspectablePromptText()
     => "Press [Inspect] to use the <color=#ffff00>access terminal</color>";
 
   public override string AInspectableUninspectableText()
-    => "Find a <color=#ffff00>keycard</color> to use the <color=#ffff00>access terminal</color>";
+    => HasBeenAccessed ? null : "Find a <color=#ffff00>keycard</color> to use the <color=#ffff00>access terminal</color>";
 
   // TODO: Refactor state
-  public override bool IsInspectable() => ElevatorDoor.Current.HasKeycard;
+  public override bool IsInspectable()
+    => ElevatorDoor.Current.HasKeycard && !HasBeenAccessed;
 }
