1a32366e758805b4332f8dee47d3afe513fa8833
Joe Anderson <joe@mousetrapped.co.uk>
2024-01-28 15:38:29 +0000

    Add used_by.rb

--- find_unused.rb
+++ find_unused.rb
@@ -1,82 +1,12 @@
-require 'yaml'
+require_relative 'usage_data'
 
-$SEARCH_DIRS = Dir.glob(
-  File.expand_path('./Assets/*', __dir__)
-).filter { |f| File.directory?(f) && File.basename(f) != 'vendor' }
+UsageData.register_source_files
 
-def find_matching_pattern(pattern)
-  $SEARCH_DIRS.flat_map do |search_dir|
-    Dir.glob(File.join(search_dir, '**', pattern))
-  end
-end
+exclude_files = File.readlines(File.expand_path('./.notunused', __dir__)).map(&:strip)
+unused_files = UsageData::SourceFile.all.reject(&:used?).reject { |f| exclude_files.include?(f.basename) }
 
-$GUID_REFERENCING_FILE_PATHS = find_matching_pattern('*.{unity,prefab,asset,mat}')
+puts "Found #{unused_files.count} unused files:"
 
-$REFENECED_GUIDS = $GUID_REFERENCING_FILE_PATHS.flat_map do |file_path|
-  File.read(file_path).scan(/guid: ([0-9a-f]+)/).flatten
-end.uniq
-
-$CS_FILE_PATHS = find_matching_pattern('*.cs')
-
-$IDENTIFIERS_REFERENCED_IN_FILES = Hash.new { |hash, key| hash[key] = [] }
-
-$CS_FILE_PATHS.each do |file_path|
-  File.read(file_path).scan(/\b[A-Z][A-Za-z]+\b/).flatten.uniq.each do |identifier|
-    $IDENTIFIERS_REFERENCED_IN_FILES[identifier] << file_path
-  end
-end
-
-$NOT_UNUSED_FILES = File.readlines(File.expand_path('./.notunused', __dir__)).map(&:strip)
-
-class SourceFile
-  attr_reader :path, :guid
-
-  def initialize(path)
-    @path = path
-    @meta_path = path + '.meta'
-    @guid = YAML.load_file(@meta_path).fetch('guid')
-  end
-
-  def used?
-    in_not_unusd? || unity_file? || guid_referenced? || (cs_file? && class_name_referenced?)
-  end
-
-  def basename
-    File.basename(@path)
-  end
-
-  def in_not_unusd?
-    $NOT_UNUSED_FILES.include?(basename)
-  end
-
-  def guid_referenced?
-    $REFENECED_GUIDS.include?(@guid)
-  end
-
-  def unity_file?
-    @path.end_with?('.unity')
-  end
-
-  def cs_file?
-    @path.end_with?('.cs')
-  end
-
-  def class_name_referenced?
-    $IDENTIFIERS_REFERENCED_IN_FILES[class_name].any? { |file| file != path }
-  end
-
-  def class_name
-    basename.split('.').first
-  end
-end
-
-$SOURCE_FILES = find_matching_pattern('*[!.meta]')
-  .filter { |f| File.file?(f) }
-  .map { |f| SourceFile.new(f) }
-
-$UNUSED_FILES = $SOURCE_FILES.reject(&:used?)
-
-puts "Found #{$UNUSED_FILES.count} unused files:"
-$UNUSED_FILES.each do |file|
+unused_files.each do |file|
   puts "  #{file.basename}"
 end
---
+++ usage_data.rb
@@ -0,0 +1,87 @@
+require 'yaml'
+
+module UsageData
+  GUID_REFERENCING_EXTENSIONS = %w[.unity .prefab .mat .asset .controller]
+  IDENTIFIER_REFERENCING_EXTENSIONS = %w[.cs]
+  IGNORE_UNUSED_EXTENSIONS = %w[.unity .preset .otf .ttf .asset .txt]
+
+  class SourceFile
+    @@by_guid = {}
+    @@all = []
+
+    def self.all
+      @@all
+    end
+
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+      @@by_guid[guid] = self
+      @@all << self
+    end
+
+    def guid
+      @guid ||= YAML.load_file(@path + '.meta').fetch('guid')
+    end
+
+    def basename
+      File.basename(@path)
+    end
+
+    def class_name
+      basename.split('.').first
+    end
+
+    def contents
+      @contents ||= File.read(@path)
+    end
+
+    def referenced_guids
+      return [] unless path.end_with?(*GUID_REFERENCING_EXTENSIONS)
+      @referenced_guids ||= contents.scan(/guid: ([0-9a-f]+)/).flatten.uniq
+    end
+
+    def referenced_identifiers
+      return [] unless path.end_with?(*IDENTIFIER_REFERENCING_EXTENSIONS)
+      @referenced_identifiers ||= contents.scan(/\b[A-Z][A-Za-z]+\b/).flatten.uniq
+    end
+
+    def uses?(file)
+      referenced_guids.include?(file.guid) || (
+        path.end_with?('.cs') &&
+        referenced_identifiers.include?(file.class_name)
+      )
+    end
+
+    def all_other_files
+      @all_other_files ||= @@all.reject { |f| f == self }
+    end
+
+    def used_by
+      all_other_files.filter { |f| f.uses?(self) }
+    end
+
+    def used?
+      path.end_with?(*IGNORE_UNUSED_EXTENSIONS) || used_by.any?
+    end
+  end
+
+  def self.search_dirs
+    @@search_dirs ||= Dir.glob(
+      File.expand_path('./Assets/*', __dir__)
+    ).filter { |f| File.directory?(f) && File.basename(f) != 'vendor' }
+  end
+
+  def self.find_matching_pattern(pattern)
+    search_dirs.flat_map do |search_dir|
+      Dir.glob(File.join(search_dir, '**', pattern))
+    end
+  end
+
+  def self.register_source_files
+    find_matching_pattern('*')
+      .filter { |f| File.file?(f) && !f.end_with?('.meta') }
+      .each { |f| SourceFile.new(f) }
+  end
+end
