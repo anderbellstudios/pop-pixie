a66b94a039bd4fa4340266bc0a60e57b7694035b
Joe Anderson <joe@mousetrapped.co.uk>
2024-04-04 10:41:16 +0100

    Configure test framework

---
+++ Assets/test/Example.test.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ExampleTest {
+  [Test]
+  public void ItWorks() {
+    Assert.IsTrue(true);
+  }
+}
