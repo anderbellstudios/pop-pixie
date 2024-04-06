231d4966697f06b48e6efd2ec59fe2a1fdb551c1
Joe Anderson <joe@mousetrapped.co.uk>
2024-04-06 13:59:47 +0100

    Add tests for entire Mission Training game

--- Assets/src/WrappedInput.cs
+++ Assets/src/WrappedInput.cs
@@ -7,29 +7,31 @@ using Rewired;
 public class WrappedInput : MonoBehaviour {
   public static bool TestMode = false;
 
-  public static Dictionary<string, bool>
-    GetButtonOverrides = new Dictionary<string, bool>(),
-    GetButtonDownOverrides = new Dictionary<string, bool>(),
-    GetButtonUpOverrides = new Dictionary<string, bool>();
+  public static Dictionary<string, Nullable<bool>>
+    GetButtonOverrides = new Dictionary<string, Nullable<bool>>(),
+    GetButtonDownOverrides = new Dictionary<string, Nullable<bool>>(),
+    GetButtonUpOverrides = new Dictionary<string, Nullable<bool>>();
+
+  public static Dictionary<string, float?> AxisOverrides = new Dictionary<string, float?>();
 
   public static Player Player {
     get => ReInput.players.GetPlayer(0);
   }
 
   public static bool GetButton(string buttonName) {
-    return Overridden(buttonName, GetButtonOverrides) || Player.GetButton(buttonName);
+    return Overridden<bool>(buttonName, GetButtonOverrides) ?? Player.GetButton(buttonName);
   }
 
   public static bool GetButtonDown(string buttonName) {
-    return Overridden(buttonName, GetButtonDownOverrides) || Player.GetButtonDown(buttonName);
+    return Overridden<bool>(buttonName, GetButtonDownOverrides) ?? Player.GetButtonDown(buttonName);
   }
 
   public static bool GetButtonUp(string buttonName) {
-    return Overridden(buttonName, GetButtonUpOverrides) || Player.GetButtonUp(buttonName);
+    return Overridden<bool>(buttonName, GetButtonUpOverrides) ?? Player.GetButtonUp(buttonName);
   }
 
   public static float GetAxis(string axis) {
-    return Player.GetAxis(axis);
+    return Overridden<float>(axis, AxisOverrides) ?? Player.GetAxis(axis);
   }
 
   public static string ControllerPrefix() {
@@ -40,10 +42,16 @@ public class WrappedInput : MonoBehaviour {
     }
   }
 
-  private static bool Overridden(string rawButtonName, Dictionary<string, bool> dict) {
+  private static Nullable<T> Overridden<T>(string rawInput, Dictionary<string, Nullable<T>> dict) where T : struct {
     if (!TestMode)
-      return false;
-    string buttonName = rawButtonName.ToLower();
-    return dict.ContainsKey(buttonName) && dict[buttonName];
+      return null;
+
+    string input = rawInput.ToLower();
+
+    if (dict.ContainsKey(input)) {
+      return dict[input];
+    }
+
+    return null;
   }
 }
---
+++ Assets/test/TrainingRoom4.test.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class TrainingRoom4Test : ABaseTest {
+  [UnitySetUp]
+  public IEnumerator Init() {
+    SceneManager.LoadScene("Training Room 4");
+    yield return null;
+    yield return AdvanceDialogue();
+    GameObject.Find("Lasers").SetActive(false);
+
+    // Make sure time passes normally while moving
+    PlayerGameObject.Current.GetComponent<ScriptedMovement>().ScriptedMovementState = false;
+  }
+
+  private IEnumerator GoAroundLevel() {
+    yield return ScriptedMovement("AfterEnemySpawn");
+
+    KillAllEnemies();
+
+    yield return ScriptedMovement(new[]{
+      "TopRight",
+      "BottomRight",
+      "BottomLeft",
+      "BeforeFinish",
+    });
+  }
+
+  [UnityTest]
+  public IEnumerator CompletesLevel() {
+    yield return GoAroundLevel();
+    yield return AwaitText("00:41", retryInterval: 0.5f, retries: 80);
+    MoveUp();
+    yield return AwaitText("unsatisfactory", regex: true);
+    yield return AdvanceDialogue();
+    ClickByText("Try again");
+    StopMoving();
+
+    yield return GoAroundLevel();
+    MoveUp();
+    yield return AwaitText("Congratulations", regex: true);
+    yield return AdvanceDialogue();
+    ClickByText("Move on");
+
+    yield return AwaitSceneChange("Training Room 5");
+  }
+}
