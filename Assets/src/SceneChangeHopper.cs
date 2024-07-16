d98b750f80a76aca76bd6295d34366a24c39a09c
Joe Anderson <joe@mousetrapped.co.uk>
2024-07-16 11:30:52 +0100

    Add intro cutscene before level 1 (#35)
    * Add intro cutscene before level 1
    
    * Fix package.json

--- Assets/src/SceneChangeHopper.cs
+++ Assets/src/SceneChangeHopper.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class SceneChangeHopper : MonoBehaviour {
   public string SceneName;
   public bool FadeOutMusic = false;
+  public float OverrideFadeOutDuration = -1f;
 
   public void Hop() {
-    SceneEvents.Current.ChangeScene(SceneName, FadeOutMusic);
+    SceneEvents.Current.ChangeScene(
+      SceneName,
+      fadeOutMusic: FadeOutMusic,
+      overrideFadeOutDuration: OverrideFadeOutDuration
+    );
   }
 }
