2f000400f0edbb3bad385d476a5c10b824be8b96
Joe Anderson <joe@mousetrapped.co.uk>
2024-09-07 18:42:57 +0100

    Debug: Do nothing in TakePercyScreenshot

--- Assets/test/TestHelpers.cs
+++ Assets/test/TestHelpers.cs
@@ -290,14 +290,15 @@ public abstract class ABaseTest {
   }
 
   protected IEnumerator TakePercyScreenshot(string name) {
-    // Cannot read pixels during a frame
-    yield return new WaitForEndOfFrame();
+    // // Cannot read pixels during a frame
+    // yield return new WaitForEndOfFrame();
 
-    Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
-    renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+    // Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
+    // renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
 
-    byte[] byteArray = renderedTexture.EncodeToPNG();
-    System.IO.File.WriteAllBytes("./Percy/" + name + ".png", byteArray);
+    // byte[] byteArray = renderedTexture.EncodeToPNG();
+    // System.IO.File.WriteAllBytes("./Percy/" + name + ".png", byteArray);
+    yield return null;
   }
 }
 #endif
