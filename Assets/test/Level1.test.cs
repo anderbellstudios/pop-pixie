7d2f38643a3b80f81ad07e3fcf62ed691ad3e6c2
Joe Anderson <joe@mousetrapped.co.uk>
2024-09-08 22:23:40 +0100

    Fix suspected flaky Percy on level 1

--- Assets/test/Level1.test.cs
+++ Assets/test/Level1.test.cs
@@ -16,6 +16,8 @@ public class Level1Test : ABaseTest {
 
   [UnityTest]
   public IEnumerator CompletesLevel() {
+    // Wait for camera to settle
+    yield return new WaitForSeconds(1f);
     yield return TakePercyScreenshot("Level1.1");
 
     yield return ScriptedMovement(new[] {
