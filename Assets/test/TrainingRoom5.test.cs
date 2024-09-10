9bc668508a4fa766982b8534ecaa496ab1743d31
Joe Anderson <joe@mousetrapped.co.uk>
2024-09-10 23:38:47 +0100

    Disable flaky room 5 Percy screenshot

--- Assets/test/TrainingRoom5.test.cs
+++ Assets/test/TrainingRoom5.test.cs
@@ -17,9 +17,11 @@ public class TrainingRoom5Test : ABaseTest {
 
   [UnityTest]
   public IEnumerator CompletesLevel() {
-    // Wait for camera to settle
-    yield return new WaitForSeconds(1f);
-    yield return TakePercyScreenshot("TrainingRoom5");
+    // Too flaky
+    // // Wait for camera to settle
+    // yield return new WaitForSeconds(1f);
+    // yield return TakePercyScreenshot("TrainingRoom5");
+
     KillAllEnemies();
     yield return AwaitSceneChange("Training Game Results Screen");
   }
