436f46f778e2ea51b0c64cf21fc2c460e4a83d68
Joe Anderson <joe@mousetrapped.co.uk>
2024-12-17 22:25:15 +0000

    Use custom build script (attempt #2) (#83)
    * Use custom build script
    
    * Fix events
    
    * Fix with -> env
    
    * Fix event
    
    * Fix event
    
    * Fix syntax
    
    * Fix
    
    * Fix description -> body
    
    * Fix cut
    
    * Fix grep
    
    * Fix output
    
    * Cancel concurrent test workflows
    
    * Fix?
    
    * Fix output
    
    * Fix output
    
    * Fix sha
    
    * Skip build if 'No build' appears in PR body
    
    * Test on push
    
    * Revert "Test on push"
    
    This reverts commit 05ecdeb8c05fe439cc38010e5ef6ce630c2e8d65.
    
    * Convert spaces to underscores
    
    * Fix grep

---
+++ Assets/src/BuildMetaData.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BuildMetaData {
+  public static String BranchName => Read("branch_name");
+  public static String CommitHash => Read("commit_hash");
+
+  public static void SetBranchName(string value)
+    => Write("branch_name", value);
+
+  public static void SetCommitHash(string value)
+    => Write("commit_hash", value);
+
+  public static void Reset() {
+    SetBranchName(null);
+    SetCommitHash(null);
+  }
+
+  private static void Write(string filename, String value) {
+    File.WriteAllText($"Assets/Resources/{filename}.txt", value ?? "");
+  }
+
+  private static String Read(string filename) {
+    string value = (Resources.Load(filename) as TextAsset).text.Trim();
+    return value == "" ? null : value;
+  }
+}
