94004aeec0a694f748d5568f10ab26443bfd6ab0
Joe Anderson <joe@mousetrapped.co.uk>
2024-12-20 12:36:28 +0000

    Update level 1 design

--- Assets/src/Door.cs
+++ Assets/src/Door.cs
@@ -1,59 +1,34 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class Door : AInspectable {
-  public bool IsOpen = false;
+public class Door : MonoBehaviour {
+  private bool _IsOpen;
+  public bool IsOpen;
+
   public PlaySound PlaySound;
-  public Transform DoorTransform;
   public BoxCollider2D DoorCollider;
-  public List<Transform> MovementPath;
-  public float MovementSpeed;
+  public SpriteRenderer OpenSprite, ClosedSprite;
 
   void Start() {
     SetIsOpen(IsOpen);
     PlaySound.Play();
-    AInspectableStart();
+  }
+
+  void Update() {
+    if (IsOpen != _IsOpen) {
+      SetIsOpen(IsOpen);
+    }
   }
 
   public void Open() => SetIsOpen(true);
   public void Close() => SetIsOpen(false);
 
-  void SetIsOpen(bool isOpen) {
-    IsOpen = isOpen;
+  private void SetIsOpen(bool isOpen) {
+    IsOpen = _IsOpen = isOpen;
     PlaySound.EventInstance.setParameterByName("Is Open", isOpen ? 1f : 0f);
-    SetDoorAngle(isOpen ? 90f : 0f);
-    SetCollider(!isOpen);
-  }
-
-  public override void OnInspect() {
-    Open();
-
-    GameObject
-      .FindWithTag("Player")
-      .GetComponent<ScriptedMovement>()
-      .FollowPath(
-        MovementPath.Select(t => t.position).ToList(),
-        MovementSpeed,
-        Close
-      );
-  }
-
-  void SetDoorAngle(float angle) {
-    DoorTransform.localRotation = Quaternion.Euler(0f, 0f, angle);
-  }
-
-  void SetCollider(bool enabled) {
-    DoorCollider.enabled = enabled;
-  }
-
-  public override bool IsInspectable() {
-    return !IsOpen;
-  }
-
-  public override String AInspectablePromptText() {
-    return "Press [Inspect] to go through door";
+    DoorCollider.enabled = !isOpen;
+    OpenSprite.enabled = isOpen;
+    ClosedSprite.enabled = !isOpen;
   }
 }
---
+++ Assets/src/TrainingGameDoor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TrainingGameDoor : AInspectable {
+  public Door Door;
+  public List<Transform> MovementPath;
+  public float MovementSpeed;
+
+  public override void OnInspect() {
+    Door.Open();
+
+    PlayerGameObject
+      .Current
+      .GetComponent<ScriptedMovement>()
+      .FollowPath(
+        MovementPath.Select(t => t.position).ToList(),
+        MovementSpeed,
+        Door.Close
+      );
+  }
+
+  public override bool IsInspectable() => !Door.IsOpen;
+
+  public override String AInspectablePromptText()
+    => "Press [Inspect] to go through door";
+}
