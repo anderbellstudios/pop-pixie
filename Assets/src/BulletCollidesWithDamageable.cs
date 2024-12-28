1b6b23859f27763f08a788c45f8e94a4bad748ee
Joe Anderson <joe@mousetrapped.co.uk>
2024-12-28 17:53:43 +0000

    Improve bullet trail accuracy

--- Assets/src/BulletCollidesWithDamageable.cs
+++ Assets/src/BulletCollidesWithDamageable.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class BulletCollidesWithDamageable : MonoBehaviour {
   public BulletData BulletData;
+  public Rigidbody2D Rigidbody;
   public Collider2D Collider;
   public GameObject Explosion;
+
+  private bool Hit = false;
+  private int ShouldDestroyInFrames = -1;
 
   void Start() {
     SetIgnoreCollisionWithSelf(true);
   }
 
-  void OnCollisionEnter2D(Collision2D col) {
-    HitPoints hp = col.gameObject.GetComponent<HitPoints>();
+  void OnCollisionEnter2D(Collision2D collision) {
+    if (Hit)
+      return;
+
+    HitPoints hp = collision.gameObject.GetComponent<HitPoints>();
 
     if (hp != null) {
       bool isCounterAttack = hp.Damage(BulletData.Damage, true);
 
       if (isCounterAttack && BulletData.Originator) {
         Vector3 toOriginator = (BulletData.Originator.transform.position - transform.position).normalized;
-        GetComponent<Rigidbody2D>().velocity = BulletData.CounterAttackSpeed * toOriginator;
+        Rigidbody.velocity = BulletData.CounterAttackSpeed * toOriginator;
         BulletData.Damage = BulletData.CounterAttackDamage;
         gameObject.layer = LayerMask.NameToLayer("PlayerBullet");
         SetIgnoreCollisionWithSelf(false);
@@ -27,8 +34,29 @@ public class BulletCollidesWithDamageable : MonoBehaviour {
       }
     }
 
-    Instantiate(Explosion, transform.position, Quaternion.identity);
-    Destroy(gameObject);
+    Hit = true;
+
+    Vector3 point = collision.GetContact(0).point;
+    Instantiate(Explosion, point, Quaternion.identity);
+
+    // Ensure bullet stays at contact point
+    gameObject.transform.position = point;
+    Rigidbody.simulated = false;
+    Collider.enabled = false;
+
+    /**
+     * Give the trail renderer time to catch up, ensuring a continuous trail
+     * that ends at the explosion.
+     */
+    ShouldDestroyInFrames = 2;
+  }
+
+  void Update() {
+    if (ShouldDestroyInFrames == 0) {
+      Destroy(gameObject);
+    } else if (ShouldDestroyInFrames > 0) {
+      ShouldDestroyInFrames--;
+    }
   }
 
   private void SetIgnoreCollisionWithSelf(bool ignore) {
