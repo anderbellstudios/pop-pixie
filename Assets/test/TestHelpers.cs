a1c4444af055c3c10ffee109eb7ad80f089dec7c
Joe Anderson <joe@mousetrapped.co.uk>
2025-02-20 09:35:39 +0000

    Use multiple resolutions for Percy screenshots

--- Assets/test/TestHelpers.cs
+++ Assets/test/TestHelpers.cs
@@ -1,18 +1,29 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 using UnityEditor.SceneManagement;
+
 using TMPro;
 
 public abstract class ABaseTest {
+  private const string DefaultTestResolution = "16:9";
+  private static readonly string[] ScreenshotResolutions = {
+    "4:3",
+    "16:9",
+    "32:9",
+  };
+
   [UnitySetUp]
   public IEnumerator CommonSetUp() {
     string runId = System.Guid.NewGuid().ToString();
@@ -22,6 +33,7 @@ public abstract class ABaseTest {
     GameData.Current.Clear();
     ConfigData.Current.Clear();
     CheckpointData.Reset();
+    SetResolution(DefaultTestResolution);
     yield return null;
   }
 
@@ -30,6 +42,7 @@ public abstract class ABaseTest {
     StopMoving();
     StopZooming();
     ClearMousePosition();
+    SetResolution("Free Aspect");
     yield return null;
   }
 
@@ -517,24 +530,86 @@ public abstract class ABaseTest {
     backgroundColor.a = 1;
     camera.backgroundColor = backgroundColor;
 
-    RenderTexture screenTexture = new RenderTexture(Screen.width, Screen.height, 16);
-    RenderTexture previousTargetTexture = camera.targetTexture;
+    foreach (string resolution in ScreenshotResolutions) {
+      SetResolution(resolution);
+      yield return null;
 
-    camera.targetTexture = screenTexture;
-    RenderTexture.active = screenTexture;
-    camera.Render();
+      RenderTexture screenTexture = new RenderTexture(Screen.width, Screen.height, 16);
+      RenderTexture previousTargetTexture = camera.targetTexture;
 
-    Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
-    renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+      camera.targetTexture = screenTexture;
+      RenderTexture.active = screenTexture;
+      camera.Render();
 
-    // Clean up
-    camera.targetTexture = previousTargetTexture;
-    RenderTexture.active = null;
+      Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
+      renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
 
-    byte[] byteArray = renderedTexture.EncodeToPNG();
-    System.IO.File.WriteAllBytes("./Percy/" + name + ".png", byteArray);
+      // Clean up
+      camera.targetTexture = previousTargetTexture;
+      RenderTexture.active = null;
+
+      string path = String.Format(
+        "./Percy/{0}-{1}.png",
+        name,
+        resolution.Replace(':', 'x')
+      );
+
+      byte[] byteArray = renderedTexture.EncodeToPNG();
+      System.IO.File.WriteAllBytes(path, byteArray);
+    }
+
+    SetResolution(DefaultTestResolution);
 
     yield return null;
+  }
+
+  private void SetResolution(string key) {
+    var GameViewSizes = typeof(Editor)
+      .Assembly
+      .GetType("UnityEditor.GameViewSizes");
+
+    var gameViewSizes = typeof(ScriptableSingleton<>)
+      .MakeGenericType(GameViewSizes)
+      .GetProperty("instance")
+      .GetValue(null, null);
+
+    var group = GameViewSizes.GetMethod("GetGroup").Invoke(
+      gameViewSizes,
+      new object[] { (int)GameViewSizeGroupType.Standalone }
+    );
+
+    string[] displayTexts = group
+      .GetType()
+      .GetMethod("GetDisplayTexts")
+      .Invoke(group, null) as string[];
+
+    int index = Array.FindIndex(displayTexts, (displayText) =>
+      displayText == key ||
+      displayText.StartsWith("Test " + key)
+    );
+
+    if (index == -1) {
+      throw new System.Exception(
+        String.Format(
+          "Resolution not found: {0}. Available resolutions: {1}",
+          key,
+          String.Join(", ", displayTexts)
+        )
+      );
+    }
+
+    var GameView = typeof(Editor)
+      .Assembly
+      .GetType("UnityEditor.GameView");
+
+    var window = EditorWindow.GetWindow(GameView);
+
+    GameView
+      .GetProperty(
+        "selectedSizeIndex",
+        BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+      )
+      .SetValue(window, index, null);
   }
 }
 #endif
