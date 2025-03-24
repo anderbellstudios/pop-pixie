47ad8834c9acc2e56726630885c0e1e6547ac2d7
Joe Anderson <joe@mousetrapped.co.uk>
2025-03-24 10:48:20 +0000

    Add prototype walk animation

--- Assets/src/MovementManager.cs
+++ Assets/src/MovementManager.cs
@@ -37,8 +37,8 @@ public class MovementManager : MonoBehaviour {
 
   void Update() {
     if (Animator != null) {
-      Animator.SetInteger("Movement Direction", VisualMovement.x > 0 ? 1 : -1);
-      Animator.SetBool("Walking", StatePermitsMovement() && VisualMovement.magnitude > 0);
+      // Animator.SetInteger("Movement Direction", VisualMovement.x > 0 ? 1 : -1);
+      // Animator.SetBool("Walking", StatePermitsMovement() && VisualMovement.magnitude > 0);
       Animator.SetFloat("Speed", VisualMovement.magnitude / Time.deltaTime);
     }
 
